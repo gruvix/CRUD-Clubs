9b26203cfaee32a330f92f15b5f4febc64f4725e
Gruvix <timmy.burton@hotmail.com>
2023-12-23 19:06:23 -0300

    Prepare index file for handlebars

---
+++ template-engine/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const expresshandlebars = require('express-handlebars');
+
+const PORT = 8000;
+const app = express();
+const handlebars = expresshandlebars.create();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    layout: 'main',
+    data: {
+
+    },
+  });
+});
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
