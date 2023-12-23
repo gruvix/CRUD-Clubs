53ac2b768111d1063cc03d33f25a662f3e75e866
Gruvix <timmy.burton@hotmail.com>
2023-12-23 19:09:38 -0300

    add path join from express to public folder (includes styles)

--- template-engine/index.js
+++ template-engine/index.js
@@ -12,6 +12,8 @@ const handlebars = expresshandlebars.create();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.render('home', {
     layout: 'main',
