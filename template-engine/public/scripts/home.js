ffb2f80018d1a52fb1cdf1c98c235d9919d3c123
Gruvix <timmy.burton@hotmail.com>
2023-12-24 12:17:12 -0300

    usernames are now longer case sensitive
    made this change to ensure users put their username right

--- template-engine/public/scripts/home.js
+++ template-engine/public/scripts/home.js
@@ -1,4 +1,4 @@
 document.querySelector('#enter-page-button').addEventListener('click', () => {
-  const userName = document.querySelector('#username').value;
+  const userName = document.querySelector('#username').value.toLowerCase();
   window.location.href = `/user/${userName}/teams`;
 });
