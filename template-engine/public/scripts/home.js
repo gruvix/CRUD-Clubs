5713f2e379f1ea214a6b97620ba904d2e35e32a2
Gruvix <timmy.burton@hotmail.com>
2023-12-25 10:43:47 -0300

    homepage now stores and restores sessions

--- template-engine/public/scripts/home.js
+++ template-engine/public/scripts/home.js
@@ -1,4 +1,26 @@
+function loadUsername() {
+  return localStorage.getItem('username');
+}
+
+function saveUsername(username) {
+  localStorage.setItem('username', username);
+}
+
+function isLoggedIn() {
+  return loadUsername() !== null;
+}
+
+function restoreSession() {
+  if (isLoggedIn()) {
+    const userName = loadUsername();
+    window.location.href = `/user/${userName}/teams`;
+  }
+}
+
+restoreSession();
+
 document.querySelector('#enter-page-button').addEventListener('click', () => {
   const userName = document.querySelector('#username').value.toLowerCase();
+  saveUsername(userName);
   window.location.href = `/user/${userName}/teams`;
 });
