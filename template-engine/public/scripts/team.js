077ccc1d1a4061a6734df2aa63a49ae9fd658a39
Gruvix <timmy.burton@hotmail.com>
2023-12-25 19:55:28 -0300

    add team data fields editing
    you can now edit team data fields and apply changes
    this wont save actual changes yet

--- template-engine/public/scripts/team.js
+++ template-engine/public/scripts/team.js
@@ -4,3 +4,31 @@ document.querySelector('#back-to-teams-button').addEventListener('click', () => 
   const userName = loadUsername();
   window.location.href = `/user/${userName}/teams`;
 });
+
+function prepareEditField(target) {
+  const text = $(target).children('span').text();
+  $(target).children('input').val(text).show();
+  $(target).children('span').hide();
+}
+
+function applyEditField(target) {
+  const input = $(target).children('input').val();
+  $(target).children('span').text(input).show();
+  $(target).children('input').hide();
+}
+
+$('#teamTable').on('click', (event) => {
+  if (event.target.classList.contains('btn-outline-warning')) {
+    $(event.target).hide();
+    $(event.target.parentElement.children[1]).show();
+    prepareEditField(event.target.parentElement.parentElement.children[1]);
+  }
+});
+
+$('#teamTable').on('click', (event) => {
+  if (event.target.classList.contains('btn-outline-success')) {
+    $(event.target).hide();
+    $(event.target.parentElement.children[0]).show();
+    applyEditField(event.target.parentElement.parentElement.children[1]);
+  }
+});
