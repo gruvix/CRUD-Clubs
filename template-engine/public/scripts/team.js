7ce8baaaee8016815f2916e3ded47d11c4229c2e
Gruvix <timmy.burton@hotmail.com>
2023-12-26 21:30:45 -0300

    aplying edit on team parameter now requests PATCH

--- template-engine/public/scripts/team.js
+++ template-engine/public/scripts/team.js
@@ -25,10 +25,30 @@ $('#teamTable').on('click', (event) => {
   }
 });
 
+function updateTeamParameter(cell) {
+  const parameter = cell.id;
+  const newValue = $(cell).children('input').val();
+  const username = loadUsername();
+  const teamId = $('#team-id').val();
+  const updatedData = {};
+  updatedData[parameter] = newValue;
+  const requestBody = JSON.stringify(updatedData);
+
+  fetch(`/user/${username}/teams/${teamId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: requestBody,
+  });
+}
+
 $('#teamTable').on('click', (event) => {
   if (event.target.classList.contains('btn-outline-success')) {
     $(event.target).hide();
     $(event.target.parentElement.children[0]).show();
-    applyEditField(event.target.parentElement.parentElement.children[1]);
+    const targetCell = event.target.parentElement.parentElement.children[1];
+    applyEditField(targetCell);
+    updateTeamParameter(targetCell);
   }
 });
