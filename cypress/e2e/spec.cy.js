d666e0bba4ea571fd55d3738f921368f6a2693d0
Gruvix <timmy.burton@hotmail.com>
2023-12-30 08:07:19 -0300

    add cypress test for login try with numeric username
    this test should result in an error showing and no login happening

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,4 +5,9 @@ describe('test the CRUD', () => {
     cy.visit('http://localhost:8000/');
   });
 
-  it('passes', () => {
+  it('should show an error indicating that the username may only contain letters', () => {
+    cy.get('#username').type('123').get('#enter-page-button').click()
+      .get('#username-error')
+      .should('contain', 'Username may only contain letters');
+  });
+
