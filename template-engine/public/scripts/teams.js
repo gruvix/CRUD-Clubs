9e77e08bd5d809a361365392669b0dab77534662
Gruvix <timmy.burton@hotmail.com>
2024-01-01 10:20:55 -0300

    add function to adjust team titles size by length

--- template-engine/public/scripts/teams.js
+++ template-engine/public/scripts/teams.js
@@ -1,5 +1,14 @@
 import { clearUsername, loadUsername } from './localStorage.js';
 
+function adjustTitles() {
+  const titles = document.querySelectorAll('h5');
+  titles.forEach((title) => {
+    if (title.offsetHeight > 50) {
+      title.style.fontSize = '100%'; // Adjust size as needed
+    }
+  });
+}
+adjustTitles();
 async function resetTeams(username, callback) {
   await fetch(`/user/${username}/reset`, {
     method: 'PATCH',
