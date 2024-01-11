c335e37965da3c026ac5170a55ececb96460b270
Gruvix <timmy.burton@hotmail.com>
2024-01-11 13:14:17 -0300

    fix upload image not sending image in correct structure

--- template-engine/public/src/teamEditor.js
+++ template-engine/public/src/teamEditor.js
@@ -116,9 +116,12 @@ $('#teamTable').on('keydown', (event) => {
 });
 function uploadImage(image) {
   const teamId = $('#team-id').val();
+  const formData = new FormData();
+  formData.append('image', image);
+
   fetch(`/user/${teamId}/upload`, {
     method: 'POST',
-    body: image,
+    body: formData,
   });
 }
 $('#upload-image-button').on('click', () => {
