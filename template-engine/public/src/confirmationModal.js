5ba324e6dcb00779b9fec0313aa90d5609e411d8
Gruvix <timmy.burton@hotmail.com>
2024-01-21 20:21:57 -0300

    Confirmation modal clears stale event reactions

--- template-engine/public/src/confirmationModal.js
+++ template-engine/public/src/confirmationModal.js
@@ -1,4 +1,8 @@
+function clearConfirmationModal() {
+ $('#confirmation-modal-button').off('click');
+}
 export default function setupConfirmationModal(confirmationText, callback) {
+  clearConfirmationModal();
   $('#modal-confirmation-text').text(confirmationText);
   $('#confirmation-modal-button').on('click', () => {
     callback();
