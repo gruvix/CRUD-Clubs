68165372672e62e3c8a97d0167ea5eaf67d13111
Gruvix <timmy.burton@hotmail.com>
2024-01-21 17:00:20 -0300

    add Player class for front-end availability
    add a class for player data, different to private player class, it contains only user-related player parameters

---
+++ template-engine/public/models/player.js
@@ -0,0 +1,8 @@
+export default class Player {
+  constructor(data) {
+    this.name = data.name;
+    this.position = data.position;
+    this.role = data.role;
+    this.nationality = data.nationality;
+  }
+}
