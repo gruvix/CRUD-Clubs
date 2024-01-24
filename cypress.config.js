9126263594fbedc937f64c035daed14420570d1d
Gruvix <timmy.burton@hotmail.com>
2024-01-24 17:21:18 -0300

    adjust cypress viewport size for optimal space usage

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportHeight: 920,
+    viewportWidth: 1600,
   },
 });
