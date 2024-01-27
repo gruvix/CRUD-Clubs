e7349cc3488cf7ae850a8e51b5813ee7f0bb1609
Gruvix <timmy.burton@hotmail.com>
2024-01-27 19:54:25 -0300

    add player fields functionality

--- template-engine/public/src/addTeam.js
+++ template-engine/public/src/addTeam.js
@@ -1,7 +1,18 @@
 import setupConfirmationModal from './confirmationModal.js';
 
+function addPlayerRow() {
+  const $table = $('#players-table');
+  const $template = $('#new-player-template');
+  const $newPlayerRow = $template.contents().clone(true);
+
+  $($table).children('thead').children().first()
+    .after($newPlayerRow);
+}
 $('#back-to-teams-button').on('click', () => {
   setupConfirmationModal('You are about to leave this page, team data will be lost', () => {
     window.location.href = '/user/teams';
   });
+});
+$('#add-player-button').on('click', () => {
+  addPlayerRow();
 });
