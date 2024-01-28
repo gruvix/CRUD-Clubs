00920efb6324d86db4a92d384473c7a4a15d453b
Gruvix <timmy.burton@hotmail.com>
2024-01-28 19:15:44 -0300

    send POST request with new team data

--- template-engine/public/src/teamAdder/submitHandler.js
+++ template-engine/public/src/teamAdder/submitHandler.js
@@ -1,6 +1,21 @@
 function getImageFile() {
   return $('#image-input').prop('files')[0];
 }
+async function sendData(teamData) {
+  const href = $('#submit-team-button').attr('href');
+  const response = await fetch(href, {
+    method: 'POST',
+    headers: { application: 'json' },
+    body: teamData,
+  });
+  if (response.redirected) {
+    window.location.href = response.url;
+  }
+  if (!response.ok) {
+    alert(`Error ${response.status}: could not add team`);
+  }
+}
 export default function submitHandler() {
   const image = getImageFile();
+  sendData({});
 }
