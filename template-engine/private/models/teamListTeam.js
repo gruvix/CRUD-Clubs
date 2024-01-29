59dc8fd17a9def9c3210cafead16e9c2165a8149
Gruvix <timmy.burton@hotmail.com>
2024-01-29 10:02:54 -0300

    Refactor teamListTeam class custom default properties

--- template-engine/private/models/teamListTeam.js
+++ template-engine/private/models/teamListTeam.js
@@ -1,11 +1,11 @@
 class TeamListTeam {
-  constructor(team) {
+  constructor(team, customCrest = false, isDefault = true, hasDefault = true) {
     this.name = team.name;
     this.id = team.id;
     this.crestUrl = team.crestUrl;
-    this.hasCustomCrest = false;
-    this.isDefault = true;
-    this.hasDefault = true;
+    this.hasCustomCrest = customCrest;
+    this.isDefault = isDefault;
+    this.hasDefault = hasDefault;
     this.lastUpdated = team.lastUpdated;
   }
 
