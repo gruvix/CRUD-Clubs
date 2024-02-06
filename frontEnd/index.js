caee2c44d0c8f78f708866c4681c94de9eb95a27
Gruvix <timmy.burton@hotmail.com>
2024-02-06 11:33:14 -0300

    Set up React app with react router for URL navigation

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+
+function App() {
+    return (
+    <BrowserRouter>
+    {/* Routing here */}
+    </BrowserRouter>
+    )
+}
+
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(<App />)
