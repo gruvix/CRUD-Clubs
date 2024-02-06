ff91b767fc5ae4d9dec44b56923fc6757d1a23ea
Gruvix <timmy.burton@hotmail.com>
2024-02-06 12:42:14 -0300

    Convert homepage styles to React-focused styling

--- frontEnd/src/components/Home/Home.jsx
+++ frontEnd/src/components/Home/Home.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 
 export default function Home() {
+  const titleStyle = {
+    color: 'rgb(255, 187, 0)',
+    padding: '10px',
+    marginTop: '50px',
+  };
+  const userInputStyle = {
+    maxWidth: '300px',
+    marginTop: '70%',
+  };
+  const userInputErrorStyle = {
+    paddingTop: '1%',
+  };
+  const noDisplayStyle = {
+    display: 'none',
+  };
   return (
     <div className="container">
       <div className="row justify-content-center align-items-center">
         <div className="col-md-6">
-          <h1 className="text-center border border-warning rounded" style="color: rgb(255, 187, 0); padding: 10px; margin-top: 50px">Timothy's Football Club CRUD</h1>
+          <h1 className="text-center border border-warning rounded" style={titleStyle}>Timothys Football Club CRUD</h1>
         </div>
       </div>
       <div className="row justify-content-center align-items-center align-self-center">
         <div className="col-md-3 my-auto">
-          <div className="input-group" style="max-width: 300px; margin-top: 250px;">
+          <div className="input-group" style={userInputStyle}>
             <input type="text" className="form-control" id="username" placeholder="Username" />
             <button type="button" id="enter-page-button" className="btn btn-outline-warning" href="MUST_BE_SET">
               Enter Page
@@ -18,10 +33,10 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="row justify-content-center align-items-center" style="padding-top: 1%;">
+      <div className="row justify-content-center align-items-center" style={userInputErrorStyle}>
         <div className="col-md-3">
           <div className="input-group">
-            <p className="alert alert-danger" role="alert" id="username-error" style="display: none;">Username may only contain letters</p>
+            <p className="alert alert-danger" role="alert" id="username-error" style={noDisplayStyle}>Username may only contain letters</p>
           </div>
         </div>
       </div>
