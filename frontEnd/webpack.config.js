68548dbbd26daeb6b9c5c7a2df6715ab5abb16e2
Gruvix <timmy.burton@hotmail.com>
2024-02-11 13:50:28 -0300

    Update webpack output config
    add publicpath for file loading
    allow crossOriginLoading  as anonymous

--- frontEnd/webpack.config.js
+++ frontEnd/webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
+    crossOriginLoading: 'anonymous',
   },
   module: {
     rules: [
