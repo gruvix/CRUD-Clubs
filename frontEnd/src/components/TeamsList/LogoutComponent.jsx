95a7715053c89a8bbacb6549079291194f8dc5d7
Gruvix <timmy.burton@hotmail.com>
2024-02-12 10:25:13 -0300

    import logout logic to react from template

---
+++ frontEnd/src/components/TeamsList/LogoutComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { apiRequestPaths, webAppPaths } from '../../paths';
+
+async function logout(okResponseCallback) {
+  const response = await fetch(apiRequestPaths.logout, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    okResponseCallback();
+  }
+}
+function logoutHandler(logoutCallback) {
+  logout(logoutCallback);
+}
+export default function LogoutComponent({ style, text }) {
+  const navigate = useNavigate();
+  const logoutCallback = () => {
+    navigate(webAppPaths.home);
+  };
+  return (
+    <button type="button" style={style} className="btn btn-shadow btn-outline-warning" id="log-out-button" onClick={() => logoutHandler(logoutCallback)}>
+      {text}
+    </button>
+  );
+}
