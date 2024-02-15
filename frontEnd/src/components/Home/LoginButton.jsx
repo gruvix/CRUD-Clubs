c894378881aaf1fa5b3afa4d2163c1df642bf0d9
Gruvix <timmy.burton@hotmail.com>
2024-02-15 20:01:59 -0300

    Refactor login button component
    Refactor for improved code organization and separation of concerns, aligning with expected React  methods

--- frontEnd/src/components/Home/LoginButton.jsx
+++ frontEnd/src/components/Home/LoginButton.jsx
@@ -4,39 +4,53 @@ import validateUsername from './usernameValidation';
 import loginErrorHandler from './loginErrorHandler';
 import { apiRequestPaths, webAppPaths } from '../../paths';
 
-async function login(username, responseOkCallback) {
-  const loginPath = document.getElementById('enter-page-button').getAttribute('href');
-  const response = await fetch(loginPath, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username }),
-  });
-  if (response.ok) {
-    responseOkCallback();
-  }
-}
-function handleLogin(loginCallback) {
-  const username = document.getElementById('username').value;
+async function login(username) {
   const error = validateUsername(username);
+
   if (!error) {
-    login(username, loginCallback);
-  } else {
-    loginErrorHandler(error);
+    const loginPath = document
+      .getElementById('enter-page-button')
+      .getAttribute('href');
+    const response = await fetch(loginPath, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Login Failed - server response ${response.status}`);
+    }
   }
+  loginErrorHandler(error);
 }
 
 export default function LoginButton() {
   const navigate = useNavigate();
-  const loginCallback = () => {
-    navigate(webAppPaths.teams);
+  const [username, setUsername] = React.useState('');
+
+  const handleLogin = async () => {
+    try {
+      await login(username);
+      navigate(webAppPaths.teams);
+    } catch (error) {
+      loginErrorHandler(error);
+    }
   };
+
   return (
     <>
-      <input type="text" className="form-control" id="username" placeholder="Username" onKeyDown={(e) => (e.key === 'Enter' ? handleLogin(loginCallback) : null)} />
-      <button type="button" id="enter-page-button" className="btn btn-outline-warning" href={apiRequestPaths.login} onClick={() => { handleLogin(loginCallback); }}>
+      <input
+        type="text"
+        className="form-control"
+        id="username"
+        placeholder="Username"
+        onKeyDown={(e) => (e.key === 'Enter' ? handleLogin() : null)}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button type="button" id="enter-page-button" className="btn btn-outline-warning" href={apiRequestPaths.login} onClick={handleLogin}>
         Enter Page
       </button>
     </>
