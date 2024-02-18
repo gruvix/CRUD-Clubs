1e1fabffdfd4d72cef2b529ce2ca466375e3b84b
Gruvix <timmy.burton@hotmail.com>
2024-02-18 10:28:44 -0300

    import and adapt team cards from template

---
+++ frontEnd/src/components/TeamsList/TeamCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import TeamCrest from './TeamCrest.jsx';
+import { webAppPaths } from '../../paths.js';
+
+export default function TeamCard({ team }) {
+  const navigate = useNavigate();
+  const cardBodyStyle = {
+    alignSelf: 'center',
+  };
+  const editButtonStyle = {
+    marginRight: '10px',
+  };
+  return (
+    <div className="card card-container" data-isDefaultTeam={team.hasDefault}>
+      <div className="card-header">
+        <h5 className="card-title team-card-title">{team.name}</h5>
+      </div>
+      <TeamCrest teamCrest={team.crestUrl} hasCustomCrest={team.hasCustomCrest} />
+      <div className="card-body" style={cardBodyStyle} id={team.id}>
+        <button
+          type="button"
+          className="btn btn-outline-warning overlay-button-dark edit"
+          onClick={() => navigate(`${webAppPaths.team.replace(':teamId', team.id)}`)}
+          style={editButtonStyle}
+        >
+          edit
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-danger overlay-button-dark delete"
+          data-bs-toggle="modal"
+          data-bs-target="#confirmationModal"
+        >
+          delete
+        </button>
+      </div>
+    </div>
+  );
+}
