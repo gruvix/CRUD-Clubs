fae68a60a56adef1bfdd7a76d690def70dba95d8
Gruvix <timmy.burton@hotmail.com>
2024-02-20 11:35:13 -0300

    add backend server login status pre-check server log

--- backEnd/template-engine/private/src/auth.js
+++ backEnd/template-engine/private/src/auth.js
@@ -1,4 +1,5 @@
 const ensureLoggedIn = (req, res, next) => {
+  console.log(`Checking login status for User ${req.session.username}`);
   if (req.session.username) {
     console.log(`User ${req.session.username} is requesting ${req.path}`);
     next();
