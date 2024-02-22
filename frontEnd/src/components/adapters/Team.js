2269c8c9dbf3ed150618d49762f8a19390dcabca
Gruvix <timmy.burton@hotmail.com>
2024-02-22 13:16:26 -0300

    create team data class for front end adapter

---
+++ frontEnd/src/components/adapters/Team.js
@@ -0,0 +1,21 @@
+export default class Team {
+  constructor(teamData) {
+    this.private = {
+      id: teamData.id,
+      crestUrl: teamData.crestUrl,
+      hasCustomCrest: teamData.hasCustomCrest,
+      lastUpdated: teamData.lastUpdated,
+      auth: teamData.auth,
+    };
+    this.public = {
+      name: teamData.name,
+      area: teamData.area,
+      address: teamData.address,
+      phone: teamData.phone,
+      website: teamData.website,
+      email: teamData.email,
+      venue: teamData.venue,
+    };
+    this.squad = teamData.squad;
+  }
+}
