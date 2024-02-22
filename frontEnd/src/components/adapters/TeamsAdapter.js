55af91c993398d3443eaea4f680c4700a8f49ab6
Gruvix <timmy.burton@hotmail.com>
2024-02-22 13:18:18 -0300

    create front-end request team adapter
    adapter takes care of preparing data for front-end user UI hydration

---
+++ frontEnd/src/components/adapters/TeamsAdapter.js
@@ -0,0 +1,30 @@
+/* eslint-disable class-methods-use-this */
+import { apiRequestPaths } from '../../paths';
+import Team from './Team';
+
+export default class TeamsAdapter {
+  async getTeamData(teamId) {
+    const response = await fetch(apiRequestPaths.team.replace(':teamId', teamId), {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    try {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      data.auth = true;
+      const teamData = new Team(data);
+      return teamData;
+    } catch (error) {
+      if (response.status === 401) {
+        return { private: { auth: false } };
+      }
+      throw error;
+    }
+  }
+
