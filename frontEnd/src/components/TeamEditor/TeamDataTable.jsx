9cb856d09713b9175d2a536b4df1039d2cda1214
Gruvix <timmy.burton@hotmail.com>
2024-02-21 22:53:23 -0300

    add team table rows generator
    generate each row for each team's key, will need adapter to properly get only partial data

--- frontEnd/src/components/TeamEditor/TeamDataTable.jsx
+++ frontEnd/src/components/TeamEditor/TeamDataTable.jsx
@@ -1,5 +1,31 @@
 import React, { useEffect } from 'react';
 
+async function createTeamDataRows(teamData) {
+  const parameterKeyStyle = {
+    textTransform: 'capitalize',
+    paddingTop: '3.5%',
+  };
+  const noDisplayStyle = {
+    display: 'none',
+  };
+  return Object.keys(teamData).map((key) => (
+    <tr className="table-dark table-bordered">
+      <td className="text-warning" style={parameterKeyStyle}>{key}</td>
+      <td id={key}>
+        <span>{/* this */}</span>
+        <input type="text" className="form-control" value="" style={noDisplayStyle} />
+      </td>
+      <td>
+        <button type="button" className="btn btn-shadow btn-outline-warning edit">
+          edit
+        </button>
+        <button type="button" className="btn btn-shadow btn-outline-success apply" style={noDisplayStyle}>
+          apply
+        </button>
+      </td>
+    </tr>
+  ));
+}
 export default function TeamDataTable({ teamData }) {
   const [teamDataRows, setTeamDataRows] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
