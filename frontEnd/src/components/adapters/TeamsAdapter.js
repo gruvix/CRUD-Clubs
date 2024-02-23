f98661bfe5c21a0ba88330c27e43b87f2203cc1d
Gruvix <timmy.burton@hotmail.com>
2024-02-23 11:04:10 -0300

    Add getTeamsData adapter

--- frontEnd/src/components/adapters/TeamsAdapter.js
+++ frontEnd/src/components/adapters/TeamsAdapter.js
@@ -28,3 +28,26 @@ export default class TeamsAdapter {
     }
   }
 
+  async getTeamsData() {
+    const response = await fetch(apiRequestPaths.teams, {
+      method: 'get',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    try {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      data.auth = true;
+      return data;
+    } catch (error) {
+      if (response.status === 401) {
+        return { auth: false };
+      }
+      throw error;
+    }
+  }
+}
