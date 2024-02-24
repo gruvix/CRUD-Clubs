95c7bd3ed385962dea33f84733f82f3d0d0031b8
Gruvix <timmy.burton@hotmail.com>
2024-02-24 12:44:48 -0300

    refactor properly toggle input and buttons on team table

--- frontEnd/src/components/TeamEditor/TeamDataTable.jsx
+++ frontEnd/src/components/TeamEditor/TeamDataTable.jsx
@@ -3,20 +3,15 @@ import React, { useEffect } from 'react';
 export default function TeamDataTable({ teamData }) {
   const [rowsTeamData, setRowsTeamData] = React.useState([]);
   const [editingRowKey, setEditingRowKey] = React.useState(null);
+  const [inputValue, setInputValue] = React.useState({});
 
-  const toggleApplyButton = (key, show) => {
-    const applyButton = document.getElementById(`apply-button-${key}`);
-    if (applyButton) {
-      applyButton.style.display = show ? 'inline' : 'none';
-    }
-  };
   const enableRowEditing = (key) => () => {
     setEditingRowKey(key);
-    toggleApplyButton(key, true);
+    setInputValue({ ...inputValue, [key]: rowsTeamData[key] });
+    document.getElementById(`input-field-${key}`).focus();
   };
-  const disableRowEditing = (key) => () => {
+  const disableRowEditing = () => () => {
     setEditingRowKey(null);
-    toggleApplyButton(key, false);
   };
 
   useEffect(() => {
@@ -26,9 +21,6 @@ export default function TeamDataTable({ teamData }) {
   const parameterKeyStyle = {
     textTransform: 'capitalize',
     paddingTop: '3.5%',
-  };
-  const noDisplayStyle = {
-    display: 'none',
   };
 
   return (
@@ -41,13 +33,13 @@ export default function TeamDataTable({ teamData }) {
                 <td className="text-warning" style={parameterKeyStyle}>{key}</td>
                 <td>
                   <span style={{ display: editingRowKey === key ? 'none' : 'inline' }}>{rowsTeamData[key]}</span>
-                  <input type="text" className="form-control" value="" style={{ display: editingRowKey === key ? 'inline' : 'none' }} />
+                  <input type="text" className="form-control" value={inputValue[key]} style={{ display: editingRowKey === key ? 'inline' : 'none' }} id={`input-field-${key}`} onChange={(e) => setInputValue({ ...inputValue, [key]: e.target.value })} />
                 </td>
                 <td>
                   <button type="button" className="btn btn-shadow btn-outline-warning edit" onClick={enableRowEditing(key)} style={{ display: editingRowKey === null ? 'inline' : 'none' }}>
                     edit
                   </button>
-                  <button type="button" className="btn btn-shadow btn-outline-success apply" onClick={disableRowEditing(key)} style={noDisplayStyle} id={`apply-button-${key}`}>
+                  <button type="button" className="btn btn-shadow btn-outline-success apply" onClick={disableRowEditing(key)} style={{ display: editingRowKey === key ? 'inline' : 'none' }} id={`apply-button-${key}`}>
                     apply
                   </button>
                 </td>
