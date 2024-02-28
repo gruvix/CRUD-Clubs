d8a27f980b07a6c0cbe4720a25b5510038486abf
Gruvix <timmy.burton@hotmail.com>
2024-02-28 09:45:06 -0300

    add tooltips to reset team button component
    add tooltips to both disabled and enabled states, explaining what it does or why it is disabled

--- frontEnd/src/components/TeamEditor/ResetTeamButton.jsx
+++ frontEnd/src/components/TeamEditor/ResetTeamButton.jsx
@@ -1,42 +1,58 @@
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 export default function ResetTeamButton({ teamId, hasDefault }) {
   if (hasDefault) {
+    const resetTooltip = (props) => (
+      <Tooltip {...props}>
+        Reset the team to its default values
+      </Tooltip>
+    );
     const buttonStyle = {
       marginTop: '25px',
     };
     return (
-      <button
-        type="button"
-        id="reset-team-button"
-        className="btn btn-shadow btn-outline-danger"
-        style={buttonStyle}
-        data-bs-toggle="modal"
-        data-bs-target="#confirmationModal"
-        data-toggle="tooltip"
-        data-placement="bottom"
-        title="Reset the team to its default values"
-      >
-        Reset to default
-      </button>
+      <OverlayTrigger placement="bottom" overlay={resetTooltip}>
+        <button
+          type="button"
+          id="reset-team-button"
+          className="btn btn-shadow btn-outline-danger"
+          style={buttonStyle}
+          data-bs-toggle="modal"
+          data-bs-target="#confirmationModal"
+          data-toggle="tooltip"
+          data-placement="bottom"
+          title="Reset the team to its default values"
+        >
+          Reset to default
+        </button>
+      </OverlayTrigger>
     );
   }
 
+  const resetTooltip = (props) => (
+    <Tooltip {...props}>
+      This team does not have default values
+    </Tooltip>
+  );
   const disabledButtonStyle = {
     marginTop: '25px',
     pointerEvents: 'auto',
   };
   return (
-    <button
-      type="button"
-      id="reset-team-button"
-      className="btn btn-shadow btn-outline-danger disabled"
-      style={disabledButtonStyle}
-      data-toggle="tooltip"
-      data-placement="bottom"
-      title="This team is custom-made and has no default values"
-    >
-      Reset to default
-    </button>
+    <OverlayTrigger placement="bottom" overlay={resetTooltip}>
+      <button
+        type="button"
+        id="reset-team-button"
+        className="btn btn-shadow btn-outline-danger disabled"
+        style={disabledButtonStyle}
+        data-toggle="tooltip"
+        data-placement="bottom"
+        title="This team is custom-made and has no default values"
+      >
+        Reset to default
+      </button>
+    </OverlayTrigger>
+
   );
 }
