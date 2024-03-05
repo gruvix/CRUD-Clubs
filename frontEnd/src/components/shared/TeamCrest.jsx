49554a3478976c69b026875e3a3f3d726edfd363
Gruvix <timmy.burton@hotmail.com>
2024-03-05 13:41:51 -0300

    refactor TeamCrest component to align with typescript

--- frontEnd/src/components/shared/TeamCrest.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function TeamCrest({ teamCrest, className }) {
-  return (
-    <img src={teamCrest} className={className} alt="team crest" id="team-crest" />
-  );
-}
