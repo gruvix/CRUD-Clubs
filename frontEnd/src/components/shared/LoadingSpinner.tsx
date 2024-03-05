8c82fd83729455af45628ef7987fe1ae88be3f82
Gruvix <timmy.burton@hotmail.com>
2024-03-05 13:42:54 -0300

    add style type definition to loading spinner component

--- frontEnd/src/components/shared/LoadingSpinner.tsx
+++ frontEnd/src/components/shared/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function LoadingSpinner({ style }) {
+export default function LoadingSpinner({ style }: { style?: React.CSSProperties }) {
   return (
     <div className="spinner-border text-primary" role="status" style={style}>
       <span className="visually-hidden">Loading...</span>
