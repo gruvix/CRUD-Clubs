3871a8f9ee166feca7a53940d8a058099cb913c0
Gruvix <timmy.burton@hotmail.com>
2024-03-08 14:44:15 -0300

    Implement add new player react functionality

--- frontEnd/src/components/TeamEditor/PlayersDataTable.tsx
+++ frontEnd/src/components/TeamEditor/PlayersDataTable.tsx
@@ -9,9 +9,11 @@ interface PlayersDataTableProps {
 }
 
 export default function PlayersDataTable({ playersData, teamId }: PlayersDataTableProps): React.ReactElement {
+  const NEW_PLAYER_ROW_KEY = -1;
   const [playerRows, setPlayerRows] = React.useState([] as Player[]);
   const [playerInputRows, setPlayerInputRows] = React.useState([] as Player[]);
   const [editingRowKey, setEditingRowKey] = React.useState(null);
+  const [newPlayerRow, setNewPlayerRow] = React.useState({} as Player);
   const navigate = useNavigate();
   const requestAdapter = new APIAdapter();
   function updateInputValue(event: React.ChangeEvent<HTMLInputElement>, index: number, parameter: string) {
@@ -55,31 +57,80 @@ export default function PlayersDataTable({ playersData, teamId }: PlayersDataTab
       alert(error);
     }
   };
+  const addPlayerRow = (newId: number) => {
+    const newPlayerData = new Player({...newPlayerRow, id: newId});
+    setPlayerRows((previousState) => [newPlayerData, ...previousState]);
+    setPlayerInputRows((previousState) => [...previousState, newPlayerData]);
+  };
+  const handleNewPlayer = () => {
+    try {
+      requestAdapter.addPlayer(teamId, newPlayerRow).then((data) => {
+        if (typeof data === 'object' && 'redirect' in data) {
+          navigate(data.redirect);
+        } else {
+          disableRowEditing();
+          addPlayerRow(data);
+        }
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+  const updateNewPlayerInput = (event: React.ChangeEvent<HTMLInputElement>, parameter: string) => {
+    setNewPlayerRow({
+      ...newPlayerRow,
+      [parameter]: event.target.value,
+    });
+  };
   useEffect(() => {
-    setPlayerRows(playersData);
-    setPlayerInputRows(playersData);
+    setPlayerRows([...playersData]);
+    setPlayerInputRows([...playersData]);
   }, [playersData]);
   return (
     <div style={{ height: '410px', overflow: 'auto' }}>
       <table className="table" id="players-table">
         <thead>
-          <tr className="table-dark" id="add-player-row" data-id="-1">
+          <tr className="table-dark" id="add-player-row">
             {
               playerKeys.map((parameter) => (
                 <td className="text-warning" key={parameter}>
                   {parameter}
-                  <input type="text" className="form-control" value="" style={{ display: editingRowKey === -1 ? 'inline' : 'none' }} data-parameter={parameter} />
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={newPlayerRow[parameter]}
+                    style={{ display: editingRowKey === NEW_PLAYER_ROW_KEY ? 'inline' : 'none' }}
+                    onChange={(event) => updateNewPlayerInput(event, parameter)}
+                  />
                 </td>
               ))
             }
             <td style={{ display: 'flex', minHeight: '80px', paddingTop: '20px' }}>
-              <button type="button" className="btn btn-shadow btn-outline-warning" id="add-player-button" style={{ maxHeight: '40px', minWidth: '120px' }}>
+              <button
+                type="button"
+                className="btn btn-shadow btn-outline-warning"
+                id="add-player-button"
+                style={{ maxHeight: '40px', minWidth: '120px', display: editingRowKey !== NEW_PLAYER_ROW_KEY ? 'inline' : 'none' }}
+                onClick={() => enableRowEditing(NEW_PLAYER_ROW_KEY)}
+              >
                 new player
               </button>
-              <button type="button" className="btn btn-shadow btn-outline-success" id="confirm-player-button" style={{ maxHeight: '40px', minWidth: '50px', marginRight: '10px', display: 'none' }}>
+              <button
+                type="button"
+                className="btn btn-shadow btn-outline-success"
+                id="confirm-player-button"
+                style={{ maxHeight: '40px', minWidth: '50px', marginRight: '10px', display: editingRowKey === NEW_PLAYER_ROW_KEY ? 'inline' : 'none' }}
+                onClick={() => handleNewPlayer()}
+              >
                 Add
               </button>
-              <button type="button" className="btn btn-shadow btn-outline-secondary" id="cancel-player-button" style={{ maxHeight: '40px', minWidth: '75px', display: 'none' }}>
+              <button
+                type="button"
+                className="btn btn-shadow btn-outline-secondary"
+                id="cancel-player-button"
+                style={{ maxHeight: '40px', minWidth: '75px', display: editingRowKey === NEW_PLAYER_ROW_KEY ? 'inline' : 'none' }}
+                onClick={disableRowEditing}
+              >
                 Cancel
               </button>
             </td>
@@ -96,7 +147,8 @@ export default function PlayersDataTable({ playersData, teamId }: PlayersDataTab
                       <input
                         type="text"
                         className="form-control"
-                        value={playerInputRows[index][parameter]} onChange={(e) => updateInputValue(e, index, parameter)}
+                        value={playerInputRows[index][parameter]}
+                        onChange={(e) => updateInputValue(e, index, parameter)}
                         style={{ display: editingRowKey === index ? 'inline' : 'none' }}
                       />
                     </td>
