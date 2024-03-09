e66f209be1f4376dfb5df776cab5ee529f27f251
Gruvix <timmy.burton@hotmail.com>
2024-03-09 18:30:21 -0300

    Reformat cypress clean reinstall

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    viewportHeight: 920,
+    viewportWidth: 1600,
+  },
+});
--- frontEnd/cypress.config.ts
+++ frontEnd/cypress.config.ts
@@ -5,7 +5,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    viewportHeight: 920,
-    viewportWidth: 1600,
   },
 });
