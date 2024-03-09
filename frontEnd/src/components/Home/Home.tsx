374e809b088a4b87a3e6cafb1b263c9ef812ec98
Gruvix <timmy.burton@hotmail.com>
2024-03-09 19:09:44 -0300

    move styles in Home component directly in elements definition

--- frontEnd/src/components/Home/Home.tsx
+++ frontEnd/src/components/Home/Home.tsx
@@ -18,39 +18,24 @@ export default function Home() {
     };
     checkLogin();
   });
-  const titleStyle = {
-    color: 'rgb(255, 187, 0)',
-    padding: '10px',
-    marginTop: '50px',
-  };
-  const userInputStyle = {
-    maxWidth: '300px',
-    marginTop: '70%',
-  };
-  const userInputErrorStyle = {
-    paddingTop: '1%',
-  };
-  const noOpacityStyle = {
-    opacity: '0',
-  };
   return (
     <div className="container">
       <div className="row justify-content-center align-items-center">
         <div className="col-md-6">
-          <h1 className="text-center border border-warning rounded" style={titleStyle}>Timothys Football Club CRUD</h1>
+          <h1 className="text-center border border-warning rounded" style={{ color: 'rgb(255, 187, 0)', padding: '10px', marginTop: '50px' }}>Timothys Football Club CRUD</h1>
         </div>
       </div>
       <div className="row justify-content-center align-items-center align-self-center">
         <div className="col-md-3 my-auto">
-          <div className="input-group" style={userInputStyle}>
+          <div className="input-group" style={{ maxWidth: '300px', marginTop: '70%' }}>
             <LoginButton />
           </div>
         </div>
       </div>
-      <div className="row justify-content-center align-items-center" style={userInputErrorStyle}>
+      <div className="row justify-content-center align-items-center" style={{ paddingTop: '1%' }}>
         <div className="col-md-3">
           <div className="input-group">
-            <p className="alert alert-danger" role="alert" id="username-error" style={noOpacityStyle}>Username may only contain letters</p>
+            <p className="alert alert-danger" role="alert" id="username-error" style={{ opacity: '0' }}>Username may only contain letters</p>
           </div>
         </div>
       </div>
