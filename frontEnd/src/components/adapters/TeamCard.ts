a93709b7e6fe6aa395e0fbe583119e698f368aa3
Gruvix <timmy.burton@hotmail.com>
2024-03-11 09:58:07 -0300

    TeamCard transform into typescript

--- frontEnd/src/components/adapters/TeamCard.ts
+++ frontEnd/src/components/adapters/TeamCard.ts
@@ -1,7 +1,13 @@
 import { BASE_API_URL } from '../../paths';
 
 export default class TeamCard {
-  constructor(teamData) {
+  id: number;
+  name: string;
+  crestUrl: string;
+  isDefault: boolean;
+  lastUpdated: string;
+  [key: string]: number | string | boolean;
+  constructor(teamData: any) {
     this.id = teamData.id;
     this.name = teamData.name;
     this.crestUrl = teamData.hasCustomCrest ? `${BASE_API_URL}${teamData.crestUrl}` : teamData.crestUrl;
