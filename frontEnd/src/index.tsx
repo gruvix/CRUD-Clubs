c33912d696cd65943d36f0990e38c71d17ac199b
Gruvix <timmy.burton@hotmail.com>
2024-03-12 15:57:27 -0300

    Apply prettier on index

--- frontEnd/src/index.tsx
+++ frontEnd/src/index.tsx
@@ -1,17 +1,17 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '../css/main.css';
-import { webAppPaths } from './paths.js';
-import Home from './components/Home/Home';
-import TeamEditor from './components/TeamEditor/TeamEditor';
-import TeamAdder from './components/TeamAdder/TeamAdder';
-import TeamsList from './components/TeamsList/TeamsList';
-import Error from './components/Error/Error';
-import NotFound from './components/NotFound';
-import UserChecker from './components/Home/UserChecker';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../css/main.css";
+import { webAppPaths } from "./paths.js";
+import LandingPage from "./components/Home/LandingPage";
+import Login from "./components/Home/Login";
+import TeamEditor from "./components/TeamEditor/TeamEditor";
+import TeamAdder from "./components/TeamAdder/TeamAdder";
+import TeamsList from "./components/TeamsList/TeamsList";
+import Error from "./components/Error/Error";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
         <Route path={webAppPaths.home} element={<Navigate to={webAppPaths.user} />} />
         <Route path={webAppPaths.user} element={<Login />} />
         <Route path={webAppPaths.teams} element={<TeamsList />} />
-        <Route path={webAppPaths.team(':teamId')} element={<TeamEditor />} />
+        <Route path={webAppPaths.team(":teamId")} element={<TeamEditor />} />
         <Route path={webAppPaths.addTeam} element={<TeamAdder />} />
         <Route path={webAppPaths.error} element={<Error />} />
         <Route path="*" element={<NotFound />} />
@@ -29,11 +29,10 @@ function App() {
   );
 }
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
