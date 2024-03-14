a34bcebd86b2044e5d54dc52f9cd5a03ffb8bd67
Gruvix <timmy.burton@hotmail.com>
2024-03-14 14:02:11 -0300

    apply prettier to backend reset endpoints

--- backEnd/src/components/routing/reset.js
+++ backEnd/src/components/routing/reset.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const { deleteUser, createUser } = require('../user');
-const { validateFile } = require('../utils');
-const { getUserTeamsListJSONPath } = require('../userPath');
+const express = require("express");
+const { deleteUser, createUser } = require("../user");
+const { validateFile } = require("../utils");
+const { getUserTeamsListJSONPath } = require("../userPath");
 const {
-  deleteTeam, cloneTeamFromDefault, copyTeamListTeam, hasTeamDefault,
-} = require('../teamStorage');
+  deleteTeam,
+  cloneTeamFromDefault,
+  copyTeamListTeam,
+  hasTeamDefault,
+} = require("../teamStorage");
 
 const router = express.Router();
 
-router.put('/all', (req, res) => {
+router.put("/all", (req, res) => {
   const { username } = req.session;
   console.log(`Resetting user ${username}`);
   try {
@@ -18,20 +21,20 @@ router.put('/all', (req, res) => {
     }
     res.status(204).send();
   } catch (error) {
-    res.status(400).send('Error resetting teams');
+    res.status(400).send("Error resetting teams");
   }
 });
 
-router.put('/:teamId', (req, res) => {
+router.put("/:teamId", (req, res) => {
   const { username } = req.session;
   const { teamId } = req.params;
   console.log(`Resetting team ${teamId} from ${username}`);
   try {
     if (!hasTeamDefault(username, teamId)) {
-      throw new Error('Team is not resettable');
+      throw new Error("Team is not resettable");
     }
     deleteTeam(username, teamId);
-    const defaultUsername = 'default';
+    const defaultUsername = "default";
     cloneTeamFromDefault(username, teamId);
     copyTeamListTeam(defaultUsername, username, teamId);
 
