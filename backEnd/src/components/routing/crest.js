ce08d4c59c52a9798eead126b95f45b19e888f88
Gruvix <timmy.burton@hotmail.com>
2024-03-14 13:19:19 -0300

    Fix custom crests not loading properly due to send file root config

--- backEnd/src/components/routing/crest.js
+++ backEnd/src/components/routing/crest.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const multer = require('multer');
-const { updateTeam } = require('../teamStorage');
-const { getUserCustomCrestFolderPath } = require('../userPath');
-const { validateFile } = require('../utils');
-const { storage, imageFilter } = require('../multerConfig.js');
-const { paths } = require('./paths');
+const express = require("express");
+const multer = require("multer");
+const { updateTeam } = require("../teamStorage");
+const { getUserCustomCrestFolderPath } = require("../userPath");
+const { validateFile } = require("../utils");
+const { storage, imageFilter } = require("../multerConfig.js");
+const { paths } = require("./paths");
 
 const uploadImage = multer({ storage, fileFilter: imageFilter });
 
 const router = express.Router();
 
-router.put('/:teamId', uploadImage.single('image'), (req, res) => {
+router.put("/:teamId", uploadImage.single("image"), (req, res) => {
   const { username } = req.session;
   const { teamId } = req.params;
   const { filename } = req.file;
@@ -25,14 +25,15 @@ router.put('/:teamId', uploadImage.single('image'), (req, res) => {
   res.status(200).json(crestUrl);
 });
 
-router.get('/:teamId/:filename', (req, res) => {
+router.get("/:teamId/:filename", (req, res) => {
   const { username } = req.session;
   const { teamId, filename } = req.params;
   const imgPath = `${getUserCustomCrestFolderPath(username, teamId)}/${filename}`;
+  console.log("sending img path: ", imgPath);
   if (!validateFile(imgPath)) {
-    res.status(404).send('Crest not found');
+    res.status(404).send("Crest not found");
   } else {
-    res.sendFile(imgPath, { root: '.' });
+    res.sendFile(imgPath);
   }
 });
 
