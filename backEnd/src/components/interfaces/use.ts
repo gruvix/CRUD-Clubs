ad97f278b3011ac142e659b76070aae223b51489
Gruvix <timmy.burton@hotmail.com>
2024-03-15 11:05:47 -0300

    add file filename interface to use interface's properties declaration

--- backEnd/src/components/interfaces/use.ts
+++ backEnd/src/components/interfaces/use.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from "express"
 
 interface SessionData {
-    username: string
+    username: string   
+}
+interface File {
+    filename: string
 }
 declare module 'express' {
     interface Request {
-        session: SessionData
+        session: SessionData,
+        file: File,
     }
 }
 export default interface use {
