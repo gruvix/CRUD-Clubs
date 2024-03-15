b00b4491b54982512320b546eae11cab8a4c0586
Gruvix <timmy.burton@hotmail.com>
2024-03-15 20:38:16 -0300

    apply prettier on team endpoint router

--- backEnd/src/components/routing/team.ts
+++ backEnd/src/components/routing/team.ts
@@ -1,13 +1,22 @@
-import express, { Request, Response } from 'express';
-import multer from 'multer';
-import { validateTeam, isTeamDefault, getTeam, updateTeam, deleteTeam, addTeam, hasTeamDefault } from '../teamStorage';
-import { storage, imageFilter } from '../multerConfig';
-import TeamExtended from '../models/TeamExtended';
+import express, { Request, Response } from "express";
+import multer from "multer";
+import {
+  validateTeam,
+  isTeamDefault,
+  getTeam,
+  updateTeam,
+  deleteTeam,
+  addTeam,
+  hasTeamDefault,
+} from "../teamStorage";
+import { storage, imageFilter } from "../multerConfig";
+import TeamExtended from "../models/TeamExtended";
 
 const uploadImage = multer({ storage, fileFilter: imageFilter });
 const router = express.Router();
-router.route('/add')
-  .post(uploadImage.single('image'), (req: any, res: Response) => {
+router
+  .route("/add")
+  .post(uploadImage.single("image"), (req: any, res: Response) => {
     const { username } = req.session;
     console.log(`User ${username} is adding a new team`);
     const { teamData } = req.body;
@@ -18,14 +27,15 @@ router.route('/add')
       res.status(200).send(id.toString());
     } catch (error) {
       console.log(`Error adding team: ${error}`);
-      res.status(400).send('Error adding team');
+      res.status(400).send("Error adding team");
     }
   });
 
-router.route('/:teamId')
+router
+  .route("/:teamId")
   .get((req: any, res: Response) => {
     const { teamId } = req.params;
-    const { username } : { username: string } = req.session;
+    const { username }: { username: string } = req.session;
     console.log(`User ${username} requested team ${teamId}`);
     if (!validateTeam(username, teamId)) {
       res.status(404).send();
@@ -35,12 +45,18 @@ router.route('/:teamId')
     const teamDefaultBool = isTeamDefault(username, teamId);
     if (teamDefaultBool) {
       console.log(`Team ${teamId} from user '${username}' is default`);
-      const defaultUsername = 'default';
+      const defaultUsername = "default";
       team = getTeam(defaultUsername, teamId);
     } else {
       team = getTeam(username, teamId);
     }
-    res.json(new TeamExtended({...team, isDefault: teamDefaultBool, hasDefault: hasTeamDefault(username, teamId)}));
+    res.json(
+      new TeamExtended({
+        ...team,
+        isDefault: teamDefaultBool,
+        hasDefault: hasTeamDefault(username, teamId),
+      })
+    );
   })
   .patch((req: any, res: Response) => {
     const { teamId } = req.params;
@@ -53,7 +69,7 @@ router.route('/:teamId')
       res.status(204).send();
     } catch (error) {
       console.log(error);
-      res.status(400).send('Error updating team parameter');
+      res.status(400).send("Error updating team parameter");
     }
   })
   .delete((req: any, res: Response) => {
@@ -63,7 +79,7 @@ router.route('/:teamId')
       deleteTeam(username, teamId);
       res.status(204).send();
     } catch (error) {
-      res.status(400).send('Error deleting team');
+      res.status(400).send("Error deleting team");
     }
   });
 
