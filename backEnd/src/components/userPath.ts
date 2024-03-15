19ad237bdd36428925f96a386d0f883dc79662ee
Gruvix <timmy.burton@hotmail.com>
2024-03-15 11:35:25 -0300

    Transform user path generators into typescript

--- backEnd/src/components/userPath.ts
+++ backEnd/src/components/userPath.ts
@@ -1,28 +1,33 @@
 /* eslint-disable max-len */
-const path = require('path');
+import path from "path";
 /**
  * @param {string} username - username of the user
  * @returns the path to the root folder of the user E.g. ./private/userData/USERNAME
  */
-function getUserRootPath(username) {
+export function getUserRootPath(username: string) {
   const projectRoot = __dirname;
-  return path.join(projectRoot, '..', 'userData', username);
+  return path.join(projectRoot, "..", "userData", username);
 }
 /**
  * @param {string} username - username of the user
  * @param {boolean} fullpath - if true, return the full path to the teams.json file of the user, if false, return the relative path
  * @returns the path to the teams.json file of the user E.g. .../USERNAME/teams.json
  */
-function getUserTeamsListJSONPath(username, fullpath = true) {
-  return fullpath ? `${getUserRootPath(username)}/teams.json` : '/teams.json';
+export function getUserTeamsListJSONPath(username: string, fullpath = true) {
+  return fullpath ? `${getUserRootPath(username)}/teams.json` : "/teams.json";
 }
 /**
  * @param {string} username - username of the user
  * @param {boolean} fullpath - if true, return the full path to the custom crests user folder, if false, return the relative path
  * @returns the path to the custom crests folder of the user E.g. .../USERNAME/customCrests
  */
-function getUserCustomCrestFolderPath(username, fullpath = true) {
-  return fullpath ? `${getUserRootPath(username)}/customCrests` : '/customCrests';
+export function getUserCustomCrestFolderPath(
+  username: string,
+  fullpath = true
+) {
+  return fullpath
+    ? `${getUserRootPath(username)}/customCrests`
+    : "/customCrests";
 }
 /**
  * @param {string} username - username of the user
@@ -30,16 +35,22 @@ function getUserCustomCrestFolderPath(username, fullpath = true) {
  * @param {boolean} fullpath - if true, return the full path to the crest image file of the teamId of the user WITHOUT filetype, if false, return the relative path
  * @returns the path to the image file of the user (without type) E.g. .../USERNAME/customCrests/TEAMID
  */
-function getUserCustomCrestIMGPath(username, teamId, fullpath = true) {
-  return fullpath ? `${getUserCustomCrestFolderPath(username)}/${teamId}` : `${getUserCustomCrestFolderPath(username, false)}/${teamId}`;
+export function getUserCustomCrestIMGPath(
+  username: string,
+  teamId: string | number,
+  fullpath = true
+) {
+  return fullpath
+    ? `${getUserCustomCrestFolderPath(username)}/${teamId}`
+    : `${getUserCustomCrestFolderPath(username, false)}/${teamId}`;
 }
 /**
  * @param {string} username - username of the user
  * @param {boolean} fullpath - if true, return the full path to the teams folder of the user, if false, return the relative path
  * @returns the path to the image file of the user E.g. .../USERNAME/teams
  */
-function getUserTeamsFolderPath(username, fullpath = true) {
-  return fullpath ? `${getUserRootPath(username)}/teams` : '/teams';
+export function getUserTeamsFolderPath(username: string, fullpath = true) {
+  return fullpath ? `${getUserRootPath(username)}/teams` : "/teams";
 }
 /**
  * @param {string} username - username of the user
@@ -47,14 +58,12 @@ function getUserTeamsFolderPath(username, fullpath = true) {
  * @param {boolean} fullpath - if true, return the full path to the teamId.json file of the user, if false, return the relative path
  * @returns the path to the image file of the user (without type) E.g. .../USERNAME/teams/teamId.json
  */
-function getUserTeamJSONPath(username, teamId, fullpath = true) {
-  return fullpath ? `${getUserTeamsFolderPath(username)}/${teamId}.json` : `${getUserTeamsFolderPath(username, false)}/${teamId}.json`;
+export function getUserTeamJSONPath(
+  username: string,
+  teamId: string | number,
+  fullpath = true
+) {
+  return fullpath
+    ? `${getUserTeamsFolderPath(username)}/${teamId}.json`
+    : `${getUserTeamsFolderPath(username, false)}/${teamId}.json`;
 }
-module.exports = {
-  getUserRootPath,
-  getUserTeamsListJSONPath,
-  getUserCustomCrestFolderPath,
-  getUserCustomCrestIMGPath,
-  getUserTeamsFolderPath,
-  getUserTeamJSONPath,
-};
