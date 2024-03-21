92e0cb81ad445dfadf90a9b72d294f2fed0a9688
Gruvix <timmy.burton@hotmail.com>
2024-03-21 09:49:24 -0300

    Enable NestJs Session in app module

--- backEnd/src/modules/app.module.ts
+++ backEnd/src/modules/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 import { TeamsController } from 'src/controllers/teams.controller';
+import { SessionModule } from 'nestjs-session';
 
 @Module({
-  imports: [],
+  imports: [
+    SessionModule.forRoot({
+      session: {
+        secret: 'keyboard-cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 60 * 60 * 24, sameSite: 'none', secure: false },
+      },
+    }),
+  ],
   controllers: [AppController, TeamsController],
   providers: [AppService],
 })
