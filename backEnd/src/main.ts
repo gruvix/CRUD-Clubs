2241857d057c0aa07c17e245e4d5b675899aa069
Gruvix <timmy.burton@hotmail.com>
2024-03-21 09:48:54 -0300

    Enable CORS in main

--- backEnd/src/main.ts
+++ backEnd/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { CLIENT_BASE_URL } from "./components/routing/paths";
 
+const corsOptions = {
+  origin: CLIENT_BASE_URL,
+  credentials: true,
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
