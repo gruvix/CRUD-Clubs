4fdb534d241eb82e21521e893b39bb91f555f52f
Gruvix <timmy.burton@hotmail.com>
2024-03-22 18:20:45 -0300

    Implement session storage on main module

--- backEnd/src/modules/app.module.ts
+++ backEnd/src/modules/app.module.ts
@@ -3,11 +3,20 @@ import { SessionModule } from 'nestjs-session';
 import { UserService } from 'src/services/user.service';
 import { TeamsController } from 'src/controllers/teams.controller';
 import { UserController } from 'src/controllers/user.controller';
+import * as session from 'express-session';
+import * as FileStore from 'session-file-store';
+import { getSessionsFolderPath } from 'src/components/userPath';
 
 @Module({
   imports: [
     SessionModule.forRoot({
       session: {
+        store: new (FileStore(session))({
+          path: getSessionsFolderPath(),
+          ttl: 60 * 60 * 24,
+          logFn: () => {},
+          reapInterval: 60 * 60,
+        }),
         secret: 'keyboard-cat',
         resave: false,
         saveUninitialized: true,
