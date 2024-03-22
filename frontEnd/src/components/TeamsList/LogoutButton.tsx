28fc4e5fa3d648728fe66d4dd9729a9b2f60cb46
Gruvix <timmy.burton@hotmail.com>
2024-03-22 17:56:33 -0300

    Logout btn component use API adapter logout request

--- frontEnd/src/components/TeamsList/LogoutButton.tsx
+++ frontEnd/src/components/TeamsList/LogoutButton.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { apiRequestPaths, webAppPaths } from '../../paths';
+import { webAppPaths } from '../../paths';
+import APIAdapter from '../adapters/APIAdapter';
 
-async function logout() {
-  const response = await fetch(apiRequestPaths.logout, {
-    method: 'post',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (response.ok || response.status === 401) {
-    return true;
-  }
-  throw new Error(`Logout Failed - server response ${response.status}`);
-}
 export default function LogoutButton({ style, text }: { style?: React.CSSProperties; text: string }) {
   const navigate = useNavigate();
+  const request = new APIAdapter();
   const handleLogout = async () => {
     try {
-      await logout();
+      request.logout();
       navigate(webAppPaths.home);
     } catch {
       alert('Error: could not logout');
