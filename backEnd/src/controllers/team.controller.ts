101e66b8c5726d4b773553559c1d4407390ae0bc
Gruvix <timmy.burton@hotmail.com>
2024-03-22 19:23:49 -0300

    Create team.controller.ts
    create endpoint controller to GET team data

---
+++ backEnd/src/controllers/team.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Param, Get, Req, UseGuards } from '@nestjs/common';
+import TeamExtended from 'src/components/models/TeamExtended';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { TeamService } from 'src/services/team.service';
+
+@Controller('user/team')
+export class TeamController {
+  constructor(private readonly teamService: TeamService) {}
+
+  @Get(':teamId')
+  @UseGuards(AuthGuard)
+  getTeam(@Req() req: Request & { session: any }, @Param() params: any): TeamExtended {
+    const { username } = req.session;
+    console.log(`User ${username} requested team ${params.teamId}`);
+    const data: TeamExtended = this.teamService.getTeamData(username, params.teamId);
+    return data;
+  }
+}
