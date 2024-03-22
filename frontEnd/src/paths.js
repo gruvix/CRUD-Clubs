647dd4e7f4fb17cc424f75dbde477e0b2abdd321
Gruvix <timmy.burton@hotmail.com>
2024-03-22 16:42:38 -0300

    apply prettier on frontend paths file

--- frontEnd/src/paths.js
+++ frontEnd/src/paths.js
@@ -1,11 +1,11 @@
-export const BASE_API_URL = 'http://localhost:3000';
+export const BASE_API_URL = "http://localhost:3000";
 
 export const webAppPaths = {
-  home: '/',
-  user: '/user',
-  teams: '/user/teams',
+  home: "/",
+  user: "/user",
+  teams: "/user/teams",
   team: (teamId) => `/user/team/${teamId}`,
-  addTeam: '/user/team/add',
+  addTeam: "/user/team/add",
   error: (code) => `/error/${code}`,
 };
 export const apiRequestPaths = {
