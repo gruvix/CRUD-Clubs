ac400fdf1d8a7ff27405e6571c9e23b1dad078a3
Gruvix <timmy.burton@hotmail.com>
2024-03-23 11:14:00 -0300

    Add Patch team endpoint to team controller

--- backEnd/src/controllers/team.controller.ts
+++ backEnd/src/controllers/team.controller.ts
@@ -15,4 +15,15 @@ export class TeamController {
     const data: TeamExtended = this.teamService.getTeamData(username, params.teamId);
     return data;
   }
+
+  @Patch(':teamId')
+  updateTeam(
+    @Req() req: Request & { session: any },
+    @Param() params: any,
+    @Body() data: TeamData,
+  ) {
+    const { username } = req.session;
+    console.log(`User ${username} is updating team ${params.teamId}`);
+    this.teamService.updateTeamData(username, params.teamId, data);
+  }
 }
