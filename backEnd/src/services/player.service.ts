440479428a6a72b58789eabec6f0cdceab239624
Gruvix <timmy.burton@hotmail.com>
2024-03-23 11:59:22 -0300

    add new player functionality to player service

--- backEnd/src/services/player.service.ts
+++ backEnd/src/services/player.service.ts
@@ -1,15 +1,28 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import Player from 'src/components/models/Player';
-import { updatePlayer, validateTeam } from 'src/components/teamStorage';
+import {
+  addPlayer,
+  updatePlayer,
+  validateTeam,
+} from 'src/components/teamStorage';
 
 @Injectable()
 export default class PlayerService {
-
-  updatePlayerData(
-    username: string,
-    teamId: string | number,
-    newData: Player,
-  ) {
+  newPlayer(username: string, teamId: string | number, player: Player) {
+    let newId: number;
+    if (!validateTeam(username, teamId)) {
+      throw new HttpException('Team not found', HttpStatus.BAD_REQUEST);
+    }
+    try {
+      newId = addPlayer(username, teamId, player);
+    } catch {
+      throw new HttpException(
+        'Server failed to add player',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  updatePlayerData(username: string, teamId: string | number, newData: Player) {
     if (!validateTeam(username, teamId)) {
       throw new HttpException('Team not found', HttpStatus.BAD_REQUEST);
     }
