a24dcd8d5b47a2e963c66922bc054a56d4ed0594
Gruvix <timmy.burton@hotmail.com>
2024-03-23 12:39:08 -0300

    apply prettier on Player controller

--- backEnd/src/controllers/player.controller.ts
+++ backEnd/src/controllers/player.controller.ts
@@ -1,42 +1,42 @@
 import {
-    Controller,
-    Patch,
-    Req,
-    Param,
-    Body,
-    UseGuards,
-    Post,
-  } from '@nestjs/common';
+  Controller,
+  Patch,
+  Req,
+  Param,
+  Body,
+  UseGuards,
+  Post,
+} from '@nestjs/common';
+import CustomRequest from 'src/components/models/CustomRequest.interface';
 import Player from 'src/components/models/Player';
-  import { AuthGuard } from 'src/guards/auth.guard';
-  import PlayerService from 'src/services/player.service';
-  
-  @UseGuards(AuthGuard)
-  @Controller('user/team/:teamId/player')
-  export default class PlayerController {
-    constructor(private readonly playerService: PlayerService) {}
+import { AuthGuard } from 'src/guards/auth.guard';
+import PlayerService from 'src/services/player.service';
 
-    @Post()
-    addPlayer(
-      @Req() req: Request & { session: any },
-      @Param() params: any,
-      @Body() data: Player,
-    ) {
-      const { username } = req.session;
-      console.log(`User ${username} is adding player to team ${params.teamId}`);
-      const newId = this.playerService.newPlayer(username, params.teamId, data);
-      return newId;
-    }
+@UseGuards(AuthGuard)
+@Controller('user/team/:teamId/player')
+export default class PlayerController {
+  constructor(private readonly playerService: PlayerService) {}
 
-    @Patch()
-    updatePlayer(
-      @Req() req: Request & { session: any },
-      @Param() params: any,
-      @Body() data: Player,
-    ) {
-      const { username } = req.session;
-      console.log(`User ${username} is updating team ${params.teamId}`);
-      this.playerService.updatePlayerData(username, params.teamId, data);
-    }
+  @Post()
+  addPlayer(
+    @Req() req: Request & { session: any },
+    @Param() params: any,
+    @Body() data: Player,
+  ) {
+    const { username } = req.session;
+    console.log(`User ${username} is adding player to team ${params.teamId}`);
+    const newId = this.playerService.newPlayer(username, params.teamId, data);
+    return newId;
   }
-  
+
+  @Patch()
+  updatePlayer(
+    @Req() req: Request & { session: any },
+    @Param() params: any,
+    @Body() data: Player,
+  ) {
+    const { username } = req.session;
+    console.log(`User ${username} is updating team ${params.teamId}`);
+    this.playerService.updatePlayerData(username, params.teamId, data);
+  }
+}
