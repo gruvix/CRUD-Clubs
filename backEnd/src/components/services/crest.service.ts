d895c436fd12668b03d4e59a7ca54c0269743c0d
Gruvix <timmy.burton@hotmail.com>
2024-03-24 17:47:01 -0300

    add updateCrest to crest service

--- backEnd/src/components/services/crest.service.ts
+++ backEnd/src/components/services/crest.service.ts
@@ -1,11 +1,25 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import CrestStorageAdapter from '../Adapters/crestStorage.adapter';
+import TeamStorageAdapter from '../Adapters/teamStorage.adapter';
+import { generateCustomCrestUrl } from '../storage/userPath';
+
 const crestStorage = new CrestStorageAdapter();
+const teamStorage = new TeamStorageAdapter();
 
 @Injectable()
 export default class CrestService {
 
     getCrest(username: string, fileName: string): Buffer {
         return crestStorage.getCrest(username, fileName);
+    }
+    updateCrest(username: string, teamId: number | string, filename: string): string {
+        crestStorage.deleteOldCrests(username, teamId, filename);
+        const crestUrl = generateCustomCrestUrl(teamId, filename);
+        const newData = {
+            crestUrl,
+            hasCustomCrest: true,
+          };
+          teamStorage.updateTeam(newData, username, teamId);
+        return crestUrl
     }
 }
