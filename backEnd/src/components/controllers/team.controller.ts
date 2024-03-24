49358f02ff391b0ea76aedcbfc4b14ec2fdd41df
Gruvix <timmy.burton@hotmail.com>
2024-03-24 10:27:00 -0300

    Improve data flow and definitions on team controller

--- backEnd/src/components/controllers/team.controller.ts
+++ backEnd/src/components/controllers/team.controller.ts
@@ -20,16 +20,11 @@ export default class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Get(':teamId')
-  getTeam(
-    @Req() req: CustomRequest,
-    @Param() params: any,
-  ): TeamExtended {
+  getTeam(@Req() req: CustomRequest, @Param() params: any): TeamExtended {
     const { username } = req.session;
-    console.log(`User ${username} requested team ${params.teamId}`);
-    const data: TeamExtended = this.teamService.getTeamData(
-      username,
-      params.teamId,
-    );
+    const { teamId } = params;
+    console.log(`User ${username} requested team ${teamId}`);
+    const data: TeamExtended = this.teamService.getTeamData(username, teamId);
     return data;
   }
 
@@ -40,7 +35,9 @@ export default class TeamController {
     @Body() data: TeamData,
   ) {
     const { username } = req.session;
-    console.log(`User ${username} is updating team ${params.teamId}`);
-    this.teamService.updateTeamData(username, params.teamId, data);
+    const { teamId } = params;
+    console.log(`User ${username} is updating team ${teamId}`);
+    this.teamService.updateTeamData(username, teamId, data);
+  }
   }
 }
