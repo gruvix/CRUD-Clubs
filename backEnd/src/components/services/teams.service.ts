77547b9217cf49136e0a56b9f22351a9aab77e19
Gruvix <timmy.burton@hotmail.com>
2024-03-26 11:10:32 -0300

    add reset teams list (the same as reset user) to teams service

--- backEnd/src/components/services/teams.service.ts
+++ backEnd/src/components/services/teams.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import TeamExtended from 'src/components/models/TeamExtended';
 import TeamStorageAdapter from 'src/components/Adapters/teamStorage.adapter';
+import UserStorageAdapter from '../Adapters/userStorage.adapter';
 
+const userStorage = new UserStorageAdapter();
 const teamsStorage = new TeamStorageAdapter();
 @Injectable()
 export default class TeamsService {
   getTeamsData(username: string): TeamExtended[] {
     const data: TeamExtended[] = teamsStorage.getTeamsList(username);
     return data;
+  }
+
+  resetTeamsList(username: string) {
+    userStorage.resetUser(username);
   }
 }
