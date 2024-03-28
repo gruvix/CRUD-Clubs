acd0fb9f2d19f0ab0ae37d49a088011bd8c2a53f
Gruvix <timmy.burton@hotmail.com>
2024-03-28 18:57:18 -0300

    Root page redirect to user page

--- frontEnd/src/app/page.tsx
+++ frontEnd/src/app/page.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { Metadata } from "next";
+import { webAppPaths } from "@/paths";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Home page",
 };
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  redirect(webAppPaths.user)
 }
