c05a59869fc5ec3e22d0ac43d0b443fd307a24d4
Gruvix <timmy.burton@hotmail.com>
2024-03-28 18:27:50 -0300

    Create loading component for user page

---
+++ frontEnd/src/app/user/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingSpinner from "@/components/shared/LoadingSpinner";
+import React from "react";
+
+export default function Loading() {<div className="d-flex justify-content-center">
+<LoadingSpinner
+  style={{ marginTop: "20%", width: "15rem", height: "15rem" }}
+/>
+</div>}
