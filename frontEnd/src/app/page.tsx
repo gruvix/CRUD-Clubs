dbbb72451c3ee00e1dfc2a2cf75251687d3d1df5
Gruvix <timmy.burton@hotmail.com>
2024-03-28 18:27:05 -0300

    add metadata to root page

--- frontEnd/src/app/page.tsx
+++ frontEnd/src/app/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home page",
+};
 export default function Page() {
   return <h1>Hello, Next.js!</h1>;
 }
