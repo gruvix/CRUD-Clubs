09f26b39addad8c23c20626abab10688afd1988e
Gruvix <timmy.burton@hotmail.com>
2024-03-30 16:51:10 -0300

    Improve team error component dynamic error handling

--- frontEnd/src/app/user/team/[teamId]/error.tsx
+++ frontEnd/src/app/user/team/[teamId]/error.tsx
@@ -1,16 +1,40 @@
 "use client";
+import TeamNotFoundError from "@/components/error/TeamNotFoundError";
+import UnauthorizedError from "@/components/error/UnauthorizedError";
 import { webAppPaths } from "@/paths";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Error({
   error,
   reset,
 }: {
-  error: Error & { digest?: string };
+  error: Error;
   reset: () => void;
 }) {
+  const [escapeButtonAction, setEscapeButtonAction] = React.useState(
+    () => (): void => {},
+  );
+  const [errorMessage, setErrorMessage] = React.useState("Unkown Error");
   const router = useRouter();
+  useEffect(() => {
+    if (error instanceof UnauthorizedError) {
+      setErrorMessage("Unauthorized access!");
+      setEscapeButtonAction(() => () => {
+        router.push(webAppPaths.home);
+      });
+    } else if (error instanceof TeamNotFoundError) {
+      setErrorMessage("Team Not Found!");
+      setEscapeButtonAction(() => () => {
+        router.push(webAppPaths.teams);
+      });
+    } else {
+      setErrorMessage(`${error}`);
+      setEscapeButtonAction(() => () => {
+        router.push(webAppPaths.teams);
+      });
+    }
+  }, []);
   return (
     <div className="container">
       <div className="row justify-content-center align-items-center">
@@ -23,11 +47,10 @@ export default function Error({
               marginTop: "50px",
             }}
           >
-            {error.toString()}
+            {errorMessage}
           </h1>
         </div>
       </div>
-
       <div className="row justify-content-center align-items-center">
         <div className="col-md-2 my-auto text-center">
           <div
@@ -38,17 +61,9 @@ export default function Error({
               type="button"
               id="go-home-button"
               className="btn btn-outline-warning"
-              onClick={() => router.push(webAppPaths.teams)}
+              onClick={() => escapeButtonAction()}
             >
               Get me out of here!
-            </button>            
-            <button
-              type="button"
-              id="go-home-button"
-              className="btn btn-outline-warning"
-              onClick={() => router.refresh()}
-            >
-              Reload Team
             </button>
           </div>
         </div>
