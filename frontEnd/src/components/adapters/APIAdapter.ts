28046b11a1957115a4cb7f846f331c748df86270
Gruvix <timmy.burton@hotmail.com>
2024-04-03 12:48:22 -0300

    Refactor APIAdapter's delete team error handling

--- frontEnd/src/components/adapters/APIAdapter.ts
+++ frontEnd/src/components/adapters/APIAdapter.ts
@@ -130,19 +130,18 @@ export default class APIAdapter {
       method: "DELETE",
       credentials: "include",
     });
-    try {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+    if (!response.ok) {
+      switch (response.status) {
+        case 403:
+          throw new UnauthorizedError();
+        case 404:
+          throw new TeamNotFoundError();
+        default:
+          throw new Error(`${response.status}`);
       }
-      const data = response;
-      return data;
-    } catch (error) {
-      const redirect = responseRedirect(response.status);
-      if (redirect) {
-        return redirect;
-      }
-      throw error;
     }
+    const data = response;
+    return data;
   }
   async resetTeamsList() {
     const response = await fetch(apiRequestPaths.teams, {
