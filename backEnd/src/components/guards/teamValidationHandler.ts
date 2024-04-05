46aa0b2dc551d6083f0dd7dfa7e2a9f3d0169724
Gruvix <timmy.burton@hotmail.com>
2024-04-05 16:59:04 -0300

    propagate asynchronisity to team guard's team validation handler

--- backEnd/src/components/guards/teamValidationHandler.ts
+++ backEnd/src/components/guards/teamValidationHandler.ts
@@ -2,11 +2,11 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 import TeamStorageAdapter from '../Adapters/teamStorage.adapter';
 
 const storage = new TeamStorageAdapter();
-export default function handleTeamValidation(
+export default async function handleTeamValidation(
   username: string,
   teamId: number | string,
-): null | Error {
-  if (!storage.validateTeam(username, teamId)) {
+): Promise<null | Error> {
+  if (!(await storage.validateTeam(username, teamId))) {
     throw new HttpException('Team not found', HttpStatus.NOT_FOUND);
   }
   return null;
