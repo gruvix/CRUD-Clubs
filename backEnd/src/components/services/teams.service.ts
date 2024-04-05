230be60b74f7dd3a1f6c192df51c1f8ac9b2795e
Gruvix <timmy.burton@hotmail.com>
2024-04-05 16:59:23 -0300

    Propagate correct type for teams list to teams service

--- backEnd/src/components/services/teams.service.ts
+++ backEnd/src/components/services/teams.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import TeamExtended from 'src/components/models/TeamExtended';
 import TeamStorageAdapter from 'src/components/Adapters/teamStorage.adapter';
 import UserStorageAdapter from '../Adapters/userStorage.adapter';
+import TeamListTeam from '../models/TeamListTeam';
 
 const userStorage = new UserStorageAdapter();
 const teamsStorage = new TeamStorageAdapter();
 @Injectable()
 export default class TeamsService {
-  async getTeamsData(username: string): Promise<TeamExtended[]> {
+  async getTeamsData(username: string): Promise<TeamListTeam[]> {
     const data = await teamsStorage.getTeamsList(username);
     return data;
   }
