dd95d8df4e5da94144786bad8165639908ccd5b7
Gruvix <timmy.burton@hotmail.com>
2024-04-05 10:51:35 -0300

    Refactor userStorage adapter for asynchronous operations

--- backEnd/src/components/Adapters/userStorage.adapter.ts
+++ backEnd/src/components/Adapters/userStorage.adapter.ts
@@ -1,35 +1,41 @@
 import { createFolder, deleteFile, validateFile } from '../storage/dataStorage';
-import { getUserRootPath, getUserTeamsFolderPath, getUserCustomCrestFolderPath, getUserTeamsListJSONPath } from '../storage/userPath';
+import {
+  getUserRootPath,
+  getUserTeamsFolderPath,
+  getUserCustomCrestFolderPath,
+  getUserTeamsListJSONPath,
+} from '../storage/userPath';
 import TeamStorageAdapter from './teamStorage.adapter';
 
 const teamStorage = new TeamStorageAdapter();
 export default class UserStorageAdapter {
-  createUser(username: string) {
+  async createUser(username: string): Promise<void> {
     try {
-      createFolder(getUserRootPath(username));
-      createFolder(getUserTeamsFolderPath(username));
-      createFolder(getUserCustomCrestFolderPath(username));
       const defaultUsername = 'default';
-      teamStorage.copyTeamList(defaultUsername, username);
+      await Promise.all([
+        createFolder(getUserRootPath(username)),
+        createFolder(getUserTeamsFolderPath(username)),
+        createFolder(getUserCustomCrestFolderPath(username)),
+        teamStorage.copyTeamList(defaultUsername, username),
+      ]);
     } catch (e) {
       throw new Error('Failed to create new user: ' + e);
     }
   }
-  deleteUser(username: string) {
+  async deleteUser(username: string): Promise<void> {
     try {
-      deleteFile(getUserRootPath(username));
+      await deleteFile(getUserRootPath(username));
     } catch {
       throw new Error('Failed to delete user');
     }
   }
-  userExists(username: string): boolean | Error {
-    if (!validateFile(getUserTeamsListJSONPath(username))) {
-      return false
+  async userExists(username: string): Promise<boolean | Error> {
+    if (!await validateFile(getUserTeamsListJSONPath(username))) {
+      return false;
     }
     return true;
   }
-  resetUser(username: string) {
-    this.deleteUser(username);
-    this.createUser(username);
+  async resetUser(username: string): Promise<void> {
+    await Promise.all([this.deleteUser(username), this.createUser(username)]);
   }
 }
