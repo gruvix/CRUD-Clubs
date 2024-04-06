d353a36c038ab7f52cbd1391cb5da1068ea967b5
Gruvix <timmy.burton@hotmail.com>
2024-04-06 10:36:35 -0300

    "Propagate missing asynchronity to team controller"

--- backEnd/src/components/controllers/team.controller.ts
+++ backEnd/src/components/controllers/team.controller.ts
@@ -22,39 +22,47 @@ export default class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Get()
-  async getTeam(@Req() req: CustomRequest, @Param() params: any): Promise<TeamExtended> {
+  async getTeam(
+    @Req() req: CustomRequest,
+    @Param() params: any,
+  ): Promise<TeamExtended> {
     const { username } = req.session;
     const { teamId } = params;
     console.log(`User ${username} requested team ${teamId}`);
-    const data = await this.teamService.getTeamData(username, teamId);
-    return data;
+    return await this.teamService.getTeamData(username, teamId);
   }
 
   @Patch()
-  updateTeam(
+  async updateTeam(
     @Req() req: CustomRequest,
     @Param() params: any,
     @Body() data: TeamData,
-  ) {
+  ): Promise<void> {
     const { username } = req.session;
     const { teamId } = params;
     console.log(`User ${username} is updating team ${teamId}`);
-    this.teamService.updateTeamData(username, teamId, data);
+    await this.teamService.updateTeamData(username, teamId, data);
   }
 
   @Delete()
-  deleteTeam(@Req() req: CustomRequest, @Param() params: any) {
+  async deleteTeam(
+    @Req() req: CustomRequest,
+    @Param() params: any,
+  ): Promise<void> {
     const { username } = req.session;
     const { teamId } = params;
     console.log(`User ${username} is deleting team ${teamId}`);
-    this.teamService.deleteTeam(username, teamId);
+    await this.teamService.deleteTeam(username, teamId);
   }
 
   @Put()
-  resetTeam(@Req() req: CustomRequest, @Param() params: any) {
+  async resetTeam(
+    @Req() req: CustomRequest,
+    @Param() params: any,
+  ): Promise<void> {
     const { username } = req.session;
     const { teamId } = params;
     console.log(`User ${username} is resetting team ${teamId}`);
-    this.teamService.resetTeam(username, teamId);
+    await this.teamService.resetTeam(username, teamId);
   }
 }
