87fbbf2cc8facf9446524ac156b280234637ba15
Gruvix <timmy.burton@hotmail.com>
2024-04-07 13:12:10 -0300

    add dataStorage deleteFile unit testing

--- backEnd/src/components/storage/__tests__/dataStorage.test.ts
+++ backEnd/src/components/storage/__tests__/dataStorage.test.ts
@@ -5,3 +5,19 @@ import * as fsPromises from 'fs/promises';
 const mockedFsPromises = fsPromises as jest.Mocked<typeof fsPromises>;
 
 const filePath = '/path/to/file';
+describe('deleteFile', () => {
+  it('should delete a file successfully', async () => {
+    mockedFsPromises.rm.mockResolvedValue(undefined as never);
+    storage.deleteFile(filePath);
+    expect(mockedFsPromises.rm).toHaveBeenCalledWith(filePath, {
+      recursive: true,
+      force: true,
+    });
+  });
+  it('should handle errors from deleting a file', async () => {
+    mockedFsPromises.rm.mockRejectedValue(new Error('Simulated Error'));
+    await expect(storage.deleteFile(filePath)).rejects.toThrow(
+      'Simulated Error',
+    );
+  });
+});
