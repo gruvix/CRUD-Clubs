267846dc731be988e010a85927b67c591bf1505e
Gruvix <timmy.burton@hotmail.com>
2024-04-08 17:01:09 -0300

    Rename teams lists mocks to improve clarity
    this will help difference mocks when adding unitary teams mocks

--- backEnd/src/components/Adapters/__tests__/teamStorage.test.ts
+++ backEnd/src/components/Adapters/__tests__/teamStorage.test.ts
@@ -5,24 +5,24 @@ import * as ds from '../../storage/dataStorage';
 const dsMock = ds as jest.Mocked<typeof ds>;
 const adapter = new TeamStorageAdapter();
 
-const defaultTeamMock = {
+const defaultTeamsListMock = {
   1: {
     isDefault: true,
   },
 };
-const nonDefaultTeamMock = {
+const nonDefaultTeamsListMock = {
     1: {
       isDefault: false,
     },
   };
 describe('isTeamDefault', () => {
   it('should return true when team is default', async () => {
-    dsMock.readJSONFile.mockResolvedValue(defaultTeamMock);
+    dsMock.readJSONFile.mockResolvedValue(defaultTeamsListMock);
 
     expect(await adapter.isTeamDefault('test', 1)).toEqual(true);
   });
   it('should return false when team is not default', async () => {
-    dsMock.readJSONFile.mockResolvedValue(nonDefaultTeamMock);
+    dsMock.readJSONFile.mockResolvedValue(nonDefaultTeamsListMock);
     expect(await adapter.isTeamDefault('test', 1)).toEqual(false);
   });
 });
