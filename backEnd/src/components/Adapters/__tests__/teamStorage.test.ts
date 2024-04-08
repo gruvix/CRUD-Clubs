522ebeac341672b0ffb8a4df860e1dad46e3dcff
Gruvix <timmy.burton@hotmail.com>
2024-04-08 17:10:54 -0300

    add unit test for getTeamsList in teamStorage adapter

--- backEnd/src/components/Adapters/__tests__/teamStorage.test.ts
+++ backEnd/src/components/Adapters/__tests__/teamStorage.test.ts
@@ -1,6 +1,8 @@
 import TeamStorageAdapter from '../teamStorage.adapter';
 jest.mock('../../storage/dataStorage');
 import * as ds from '../../storage/dataStorage';
+import FileNotFoundError from '../../errors/FileNotFoundError';
+jest.spyOn(console, 'log').getMockImplementation();
 
 const dsMock = ds as jest.Mocked<typeof ds>;
 const adapter = new TeamStorageAdapter();
@@ -24,5 +26,15 @@ describe('isTeamDefault', () => {
   it('should return false when team is not default', async () => {
     dsMock.readJSONFile.mockResolvedValue(nonDefaultTeamsListMock);
     expect(await adapter.isTeamDefault('test', 1)).toEqual(false);
+  });
+});
+describe('getTeamsList', () => {
+  it('should return a list of teams', async () => {
+    dsMock.readJSONFile.mockResolvedValue(defaultTeamsListMock);
+    expect(await adapter.getTeamsList('test')).toEqual(defaultTeamsListMock);
+  });
+  it('should handle errors', async () => {
+    dsMock.readJSONFile.mockRejectedValue(new FileNotFoundError());
+    await expect(adapter.getTeamsList('test')).rejects.toThrow(FileNotFoundError);
   });
 });
