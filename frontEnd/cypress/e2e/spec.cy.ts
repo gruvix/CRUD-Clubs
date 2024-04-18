f1852b240185708b8f856bdb11cf4d5c70b1be9c
Gruvix <timmy.burton@hotmail.com>
2024-04-18 11:51:53 -0300

    fix UX test not overwriting input field correctly

--- frontEnd/cypress/e2e/spec.cy.ts
+++ frontEnd/cypress/e2e/spec.cy.ts
@@ -121,13 +121,13 @@ describe("test the team editor page with the first team", () => {
 
   it("updates all team parameters with a random string", () => {
     const randomString = generateRandomString();
-    cy.wait(1000);
     cy.get("#team-table .edit").each(($editButton, index) => {
       cy.wrap($editButton)
         .click()
         .parent()
         .parent()
         .find("input")
+        .clear()
         .type(randomString)
         .get("#team-table .apply")
         .then(($applyButton) => {
