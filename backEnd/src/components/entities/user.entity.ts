2731f45c128432fc55bc85aa0d202b1efa37c240
Gruvix <timmy.burton@hotmail.com>
2024-05-06 13:18:32 -0300

    Create user entity for database storing

---
+++ backEnd/src/components/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, OneToMany, CreateDateColumn, Column } from 'typeorm';
+import Team from './team.entity';
+
+@Entity()
+export default class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ type: 'date' })
+    createdAt: Date;
+
+    @CreateDateColumn({ type: 'date' })
+    updatedAt: Date;
+    
+    @OneToMany(() => Team, (team) => team.user)
+    teams: Team[];
+}
