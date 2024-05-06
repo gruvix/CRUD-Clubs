852cce4d96851d43ed6093ceb64118fa11e2190c
Gruvix <timmy.burton@hotmail.com>
2024-05-06 15:09:02 -0300

    add cascade flag to user-teams relation

--- backEnd/src/components/entities/user.entity.ts
+++ backEnd/src/components/entities/user.entity.ts
@@ -1,23 +1,29 @@
-import { Entity, PrimaryGeneratedColumn, OneToMany, CreateDateColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  Column,
+} from 'typeorm';
 import Team from './team.entity';
 
 @Entity()
 export default class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    username: string;
+  @Column()
+  username: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @CreateDateColumn({ type: 'date' })
-    createdAt: Date;
+  @CreateDateColumn({ type: 'date' })
+  createdAt: Date;
 
-    @CreateDateColumn({ type: 'date' })
-    updatedAt: Date;
-    
-    @OneToMany(() => Team, (team) => team.user)
-    teams: Team[];
+  @CreateDateColumn({ type: 'date' })
+  updatedAt: Date;
+
+  @OneToMany(() => Team, (team) => team.user, { cascade: true })
+  teams: Team[];
 }
