713224876c2c738ea6147fb69d413d550c1f5ab5
Gruvix <timmy.burton@hotmail.com>
2024-05-08 12:50:13 -0300

    Fix models being imported instead of entities

--- backEnd/src/components/modules/dataBase.module.ts
+++ backEnd/src/components/modules/dataBase.module.ts
@@ -1,18 +1,18 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import User from "../entities/user.entity";
-import Team from "../models/Team";
-import Player from "../models/Player";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from '../entities/user.entity';
+import Team from '../entities/team.entity';
+import Player from '../entities/player.entity';
 
 @Module({
-    imports: [
-        TypeOrmModule.forRoot({
-            type: 'better-sqlite3',
-            database: './src/userData/userData.db',
-            entities: [User, Team, Player],
-            synchronize: process.env.PRODUCTION === 'false' ? true : false,
-            logging: true,
-          })
-    ],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: './src/userData/userData.db',
+      entities: [User, Team, Player],
+      synchronize: process.env.PRODUCTION === 'false' ? true : false,
+      logging: true,
+    }),
+  ],
 })
 export default class DataBaseModule {}
