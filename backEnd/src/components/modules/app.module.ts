77797a0c69e57e6a6c8d48f568b446764a5d1fe3
Gruvix <timmy.burton@hotmail.com>
2024-05-08 12:39:57 -0300

    reorder app module imports for improved clarity

--- backEnd/src/components/modules/app.module.ts
+++ backEnd/src/components/modules/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import PlayerService from 'src/components/services/player.service';
 import SessionModule from './session.module';
 import DataBaseModule from './dataBase.module';
 
@@ -7,16 +6,16 @@ import UserController from 'src/components/controllers/user.controller';
 import TeamsController from 'src/components/controllers/teams.controller';
 import TeamController from 'src/components/controllers/team.controller';
 import PlayerController from 'src/components/controllers/player.controller';
-import TeamService from 'src/components/services/team.service';
-import TeamsService from 'src/components/services/teams.service';
-import UserService from 'src/components/services/user.service';
 import NewTeamController from '../controllers/newTeam.controller';
 import CrestController from '../controllers/crest.controller';
+
+import UserService from 'src/components/services/user.service';
+import PlayerService from 'src/components/services/player.service';
+import TeamService from 'src/components/services/team.service';
+import TeamsService from 'src/components/services/teams.service';
 import CrestService from '../services/crest.service';
-import Team from '../entities/team.entity';
+
 import 'dotenv/config';
-import User from '../entities/user.entity';
-import Player from '../entities/player.entity';
 
 @Module({
   imports: [
