21713c5bece876d9a566bbe196da30b57a53871d
Gruvix <timmy.burton@hotmail.com>
2024-05-09 11:05:23 -0300

    Refactor seedData service to align with SOLID principles
    split entities creation into players, teams and user respectively
    Fix path generated
    remove unnecessary repositories injections, as team and players are stored via cascade effect when user is stored
    remove unused models imports

--- backEnd/src/components/services/seedData.service.ts
+++ backEnd/src/components/services/seedData.service.ts
@@ -5,18 +5,12 @@ import { readJSONFile } from '../storage/dataStorage';
 import User from '../entities/user.entity';
 import Team from '../entities/team.entity';
 import Player from '../entities/player.entity';
-import TeamExtended from '../models/TeamExtended';
-import * as PlayerJSON from '../models/Player';
 
 @Injectable()
 export default class SeedDataService implements OnModuleInit {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-    @InjectRepository(Team)
-    private readonly teamRepository: Repository<Team>,
-    @InjectRepository(Player)
-    private readonly playerRepository: Repository<Player>,
   ) {}
   async onModuleInit() {
     const valueExists = await this.userRepository
@@ -28,53 +22,63 @@ export default class SeedDataService implements OnModuleInit {
   }
 
   private async seedBaseTeams(): Promise<void> {
-    const baseUser = await this.generateUserFromJSON();
-    await this.userRepository.manager.transaction(
-      async (transactionalEntityManager) => {
-        await transactionalEntityManager.save(baseUser);
-      },
-    );
+    const JSONFolderPath = process.env.BASE_USER_FOLDER_PATH;
+    const baseUser = await this.generateUserFromJSON(JSONFolderPath);
+    const manager = this.userRepository.manager;
+    await manager.transaction(async (transactionalEntityManager) => {
+      await transactionalEntityManager.save(baseUser);
+    });
   }
 
   private async generateUserFromJSON(
+    JSONFolderPath: string,
     username: string = 'default',
     password: string = 'default',
   ): Promise<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    const JSONFolderPath = process.env.BASE_USER_FOLDER_PATH;
-    const teamsJSON = await readJSONFile(JSONFolderPath);
+    user.teams = [];
 
-    teamsJSON.forEach(async (team: TeamExtended) => {
-      const newTeam = new Team();
+    const teamsJSON = await readJSONFile(JSONFolderPath + '/teams.json');
+    for (const teamId of Object.keys(teamsJSON)) {
       const teamJSON = await readJSONFile(
-        JSONFolderPath + '/' + team.id + '.json',
+        JSONFolderPath + '/teams/' + teamId + '.json',
       );
-      newTeam.id = teamJSON.id;
-      newTeam.name = teamJSON.name;
-      newTeam.area = teamJSON.area.name;
-      newTeam.address = teamJSON.address;
-      newTeam.phone = teamJSON.phone;
-      newTeam.website = teamJSON.website;
-      newTeam.email = teamJSON.email;
-      newTeam.venue = teamJSON.venue;
-      newTeam.crestUrl = teamJSON.crestUrl;
-      newTeam.hasCustomCrest = teamJSON.hasCustomCrest;
-      newTeam.squad = [];
-      newTeam.updatedAt = teamJSON.lastUpdated;
-
-      const players = teamJSON.squad;
-      players.forEach(async (player: PlayerJSON.default) => {
-        const newPlayer = new Player();
-        newPlayer.id = player.id;
-        newPlayer.name = player.name;
-        newPlayer.position = player.position || player.role;
-        newPlayer.nationality = player.nationality;
-        newTeam.squad.push(newPlayer);
-      });
+      const newTeam = await this.createTeamFromJSON(teamJSON);
       user.teams.push(newTeam);
-    });
+    }
     return user;
+  }
+
+  private async createTeamFromJSON(teamJSON: any): Promise<Team> {
+    const newTeam = new Team();
+    newTeam.id = teamJSON.id;
+    newTeam.name = teamJSON.name;
+    newTeam.area = teamJSON.area.name;
+    newTeam.address = teamJSON.address;
+    newTeam.phone = teamJSON.phone;
+    newTeam.website = teamJSON.website;
+    newTeam.email = teamJSON.email;
+    newTeam.venue = teamJSON.venue;
+    newTeam.crestUrl = teamJSON.crestUrl;
+    newTeam.hasCustomCrest = teamJSON.hasCustomCrest || false;
+    newTeam.squad = [];
+    newTeam.updatedAt = teamJSON.lastUpdated;
+
+    const players: PlayerJSON[] = teamJSON.squad;
+    for (const player of players) {
+      const newPlayer = await this.createPlayerFromJSON(player);
+      newTeam.squad.push(newPlayer);
+    }
+    return newTeam;
+  }
+  private async createPlayerFromJSON(playerJSON: PlayerJSON): Promise<Player> {
+    const newPlayer = new Player();
+    newPlayer.id = playerJSON.id;
+    newPlayer.name = playerJSON.name;
+    newPlayer.position = playerJSON.position || playerJSON.role;
+    newPlayer.nationality = playerJSON.nationality;
+    return newPlayer;
   }
 }
