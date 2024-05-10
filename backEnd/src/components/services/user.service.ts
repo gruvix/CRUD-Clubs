b681284c40ca4cf59b2b366f85e8ec43ab482180
Gruvix <timmy.burton@hotmail.com>
2024-05-10 11:07:58 -0300

    Refactor user service operations to TypeORM transactions
    No longer uses userStorage adapter, instead handles operations with TypeORM entities repositories
    replacing all filesystem operations with database transactions

--- backEnd/src/components/services/user.service.ts
+++ backEnd/src/components/services/user.service.ts
@@ -1,32 +1,84 @@
-
 import { Injectable } from '@nestjs/common';
-import { validateUsername } from 'src/components/userValidation';
-import UserStorageAdapter from 'src/components/Adapters/userStorage.adapter';
+import { InjectRepository } from '@nestjs/typeorm';
+import { isUsernameValid } from 'src/components/userValidation';
+import { Repository } from 'typeorm';
 import CustomRequest from '../models/CustomRequest.interface';
-
-const userStorage = new UserStorageAdapter();
-function getUsername(req: CustomRequest): string {
-  return req.session.username;
-}
+import User from '../entities/user.entity';
+import Team from '../entities/team.entity';
+import Player from '../entities/player.entity';
 
 @Injectable()
 export default class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Team)
+    private readonly teamRepository: Repository<Team>,
+  ) {}
 
   isLoggedIn(req: CustomRequest): boolean {
-    const username = getUsername(req);
+    const username = req.session.username;
     return !!username;
   }
-
-  async handleUserLogin(username: string) {
-    const error = validateUsername(username);
-    if (error) {
-      throw error;
+  private copyPlayersToTeam(
+    team: Team,
+    players: Player[],
+  ): void {
+    for (const player of players) {
+      player.team = team;
+      delete player.id;
+      team.squad.push(player);
     }
-    const exists = await userStorage.userExists(username);
-    if (!exists) {
+  }
+
+  private async copyTeamsToUser(user: User, teams: Team[]): Promise<void> {
+    for (const team of teams) {
+      team.user = user.id;
+      const players: Player[] = (
+        await this.teamRepository.manager.findOne(Team, {
+          where: { user: user.id },
+          relations: ['squad'],
+        })
+      ).squad;
+      delete team.id;
+      team.squad = [];
+      this.copyPlayersToTeam(team, players);
+      user.teams.push(team);
+    }
+  }
+
+  private async createNewUser(
+    username: string,
+    password: string = 'default',
+  ): Promise<void> {
+    const newUser = new User();
+    newUser.username = username;
+    newUser.password = password;
+    newUser.teams = [];
+
+    this.userRepository.manager.transaction(
+      async (transactionalEntityManager) => {
+        let savedUser = await transactionalEntityManager.save(newUser);
+
+        const baseTeams = (
+          await transactionalEntityManager.findOne(User, {
+            where: { username: 'default' },
+            relations: ['teams'],
+          })
+        ).teams;
+        await this.copyTeamsToUser(savedUser, baseTeams);
+        await transactionalEntityManager.save(savedUser);
+      },
+    );
+  }
+
+  async handleUserLogin(username: string): Promise<boolean> {
+    if (!isUsernameValid(username)) return false;
+    const user = await this.userRepository.findOneBy({ username });
+    if (!user) {
       try {
         console.log('user not found, creating new user');
-        await userStorage.createUser(username);
+        await this.createNewUser(username);
         return true;
       } catch (error) {
         throw error;
