7ada575327caceed85b40656e2cab63eefc04739
Gruvix <timmy.burton@hotmail.com>
2024-05-11 17:33:21 -0300

    Migrate main app controllers & services to football Module
    All football related controllers and services export to custom module for better organization

--- backEnd/src/components/modules/app.module.ts
+++ backEnd/src/components/modules/app.module.ts
@@ -1,23 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import SessionModule from './session.module';
 import DatabaseModule from './database.module';
-
-import UserController from 'src/components/controllers/user.controller';
-import TeamsController from 'src/components/controllers/teams.controller';
-import TeamController from 'src/components/controllers/team.controller';
-import PlayerController from 'src/components/controllers/player.controller';
-import NewTeamController from '../controllers/newTeam.controller';
-import CrestController from '../controllers/crest.controller';
-
-import UserService from 'src/components/services/user.service';
-import PlayerService from 'src/components/services/player.service';
-import TeamService from 'src/components/services/team.service';
-import TeamsService from 'src/components/services/teams.service';
-import CrestService from '../services/crest.service';
-
 import 'dotenv/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import UserModule from './user.module';
+import FootBallModule from './football.module';
 
 @Module({
   imports: [
@@ -31,19 +18,9 @@ import UserModule from './user.module';
       logging: true,
     }),
     UserModule,
+    FootBallModule,
   ],
-  controllers: [
-    TeamsController,
-    TeamController,
-    PlayerController,
-    NewTeamController,
-    CrestController,
-  ],
-  providers: [
-    TeamsService,
-    TeamService,
-    PlayerService,
-    CrestService,
-  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
