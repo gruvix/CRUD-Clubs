672ec730d5de49cb2afc9611adee5afc04df168f
Gruvix <timmy.burton@hotmail.com>
2024-05-13 11:11:46 -0300

    Create class for main team data

---
+++ backEnd/src/components/models/TeamData.ts
@@ -0,0 +1,20 @@
+export default class TeamData {
+  name: string;
+  area: string;
+  address: string;
+  phone: string | number;
+  website: string;
+  email: string;
+  venue: string;
+  readonly hasDefault: boolean;
+  constructor(data: TeamData) {
+    this.name = data.name;
+    this.area = data.area;
+    this.address = data.address;
+    this.phone = data.phone;
+    this.website = data.website;
+    this.email = data.email;
+    this.venue = data.venue;
+    this.hasDefault = data.hasDefault;
+  }
+}
