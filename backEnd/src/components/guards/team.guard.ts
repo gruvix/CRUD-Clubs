304d8601393ae464faca4c30c87b24ecac8dc6d3
Gruvix <timmy.burton@hotmail.com>
2024-05-13 11:05:49 -0300

    Integrate isTeamValid into team guard for ORM use adaptation
    isTeamValid use typeORM for validation instead of teamStorage adapter
    this means it requires repository injection
    Integrated into team guard class to streamline repository injection process

--- backEnd/src/components/guards/team.guard.ts
+++ backEnd/src/components/guards/team.guard.ts
@@ -5,23 +5,43 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import isTeamValid from './isTeamValid';
 import isNonNegativeNumber from './isNonNegativeNumber';
+import UserService from '../services/user.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import Team from '../entities/team.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TeamGuard implements CanActivate {
+  constructor(
+    private readonly userService: UserService,
+    @InjectRepository(Team)
+    private readonly teamRepository: Repository<Team>,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     request.params.teamId = Number(request.params.teamId);
     const teamId = request.params.teamId;
     const username = request.session.username;
+    const userId = await this.userService.getUserId(username);
     if (!isNonNegativeNumber(teamId)) {
-      console.log(`User ${username} tried to access team ${teamId}, but the ID is invalid`);
+      console.log(
+        `User ${username} tried to access team ${teamId}, but the ID is invalid`,
+      );
       throw new HttpException('Invalid team id', HttpStatus.BAD_REQUEST);
     }
-    if (await isTeamValid(username, teamId)) {
-      console.log(`User ${username} tried to access team ${teamId}, but it doesn't exist`);
+    if (await this.isTeamValid(userId, teamId)) {
+      console.log(
+        `User ${username} tried to access team ${teamId}, but it doesn't exist`,
+      );
       throw new HttpException('Team not found', HttpStatus.NOT_FOUND);
+    }
+    return true;
+  }
+
+  private async isTeamValid(userId: number, teamId: number): Promise<boolean> {
+    if (await this.teamRepository.findOneBy({ id: teamId, user: userId })) {
+      return false;
     }
     return true;
   }
