1d6c814599b7083c4b1e0ce08e695df76dec0b29
Gruvix <timmy.burton@hotmail.com>
2024-05-13 12:46:26 -0300

    refactor team existance validation on team guard
    replace isTeamValid with teamExistsOnUser
    instead of checking if a team exists, it will specifically check if the team exists on the given user, disabling the posibility of cross-user data retrieval, increasing overall security

--- backEnd/src/components/guards/team.guard.ts
+++ backEnd/src/components/guards/team.guard.ts
@@ -8,8 +8,8 @@ import {
 import isNonNegativeNumber from './isNonNegativeNumber';
 import UserService from '../services/user.service';
 import { InjectRepository } from '@nestjs/typeorm';
-import Team from '../entities/team.entity';
 import { Repository } from 'typeorm';
+import Team from '../entities/team.entity';
 
 @Injectable()
 export class TeamGuard implements CanActivate {
@@ -23,26 +23,41 @@ export class TeamGuard implements CanActivate {
     request.params.teamId = Number(request.params.teamId);
     const teamId = request.params.teamId;
     const username = request.session.username;
-    const userId = await this.userService.getUserId(username);
+
     if (!isNonNegativeNumber(teamId)) {
       console.log(
         `User ${username} tried to access team ${teamId}, but the ID is invalid`,
       );
       throw new HttpException('Invalid team id', HttpStatus.BAD_REQUEST);
     }
-    if (await this.isTeamValid(userId, teamId)) {
+
+    const userId = await this.userService.getUserId(username);
+    if (await this.teamExistsOnUser(userId, teamId)) {
       console.log(
-        `User ${username} tried to access team ${teamId}, but it doesn't exist`,
+        `User ${username} tried to access team ${teamId}, but there is no team related to userId ${userId}`,
       );
       throw new HttpException('Team not found', HttpStatus.NOT_FOUND);
     }
     return true;
   }
 
-  private async isTeamValid(userId: number, teamId: number): Promise<boolean> {
-    if (await this.teamRepository.findOneBy({ id: teamId, user: userId })) {
-      return false;
+  private async teamExistsOnUser(
+    userId: number,
+    teamId: number,
+  ): Promise<boolean> {
+    try {
+      const team = await this.teamRepository
+        .createQueryBuilder('team')
+        .where('team.id = :teamId', { teamId })
+        .andWhere('team.user = :userId', { userId })
+        .getOne();
+
+      if (team) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      throw new Error('Failed to validate team: ' + error);
     }
-    return true;
   }
 }
