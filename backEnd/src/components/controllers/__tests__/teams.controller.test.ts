47bcfbb54cce6db5e8efe631f0918bf0eff5fdf6
Gruvix <timmy.burton@hotmail.com>
2024-05-20 12:04:53 -0300

    Initialize teams controller jest tests environment
    prepare mocked services and mock constants for testing

---
+++ backEnd/src/components/controllers/__tests__/teams.controller.test.ts
@@ -0,0 +1,35 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import TeamsService from '@comp/services/teams.service';
+import TeamsController from '@comp/controllers/teams.controller';
+import TeamListTeam from '@comp/models/TeamListTeam';
+import CustomRequest from '@comp/interfaces/CustomRequest.interface';
+import Team from '@comp/entities/team.entity';
+import UserService from '@comp/services/user.service';
+import Team from '@comp/entities/team.entity';
+import User from '@comp/entities/user.entity';
+
+describe('TeamsController', () => {
+  let teamsController: TeamsController;
+  let teamsService: TeamsService;
+  let userService: UserService;
+
+  const username = 'test';
+  const userId = 1;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [TeamsController],
+      providers: [
+        TeamsService,
+        UserService,
+        { provide: getRepositoryToken(Team), useValue: jest.fn() },
+        { provide: getRepositoryToken(User), useValue: jest.fn() },
+      ],
+    }).compile();
+
+    teamsService = module.get<TeamsService>(TeamsService);
+    userService = module.get<UserService>(UserService);
+    teamsController = module.get<TeamsController>(TeamsController);
+  });
+});
