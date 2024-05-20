7e7ffaf4d91453331cd261548d0cab1edd300730
Gruvix <timmy.burton@hotmail.com>
2024-05-20 17:05:31 -0300

    update teams controller tests with updated decorators usage

--- backEnd/src/components/controllers/__tests__/teams.controller.test.ts
+++ backEnd/src/components/controllers/__tests__/teams.controller.test.ts
@@ -3,7 +3,6 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import TeamsService from '@comp/services/teams.service';
 import TeamsController from '@comp/controllers/teams.controller';
-import CustomRequest from '@comp/interfaces/CustomRequest.interface';
 import UserService from '@comp/services/user.service';
 import Team from '@comp/entities/team.entity';
 import User from '@comp/entities/user.entity';
@@ -15,10 +14,6 @@ describe('TeamsController', () => {
 
   const username = 'test';
   const userId = 1;
-
-  const mockRequest = {
-    session: { username, userId },
-  } as CustomRequest;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -50,7 +45,7 @@ describe('TeamsController', () => {
         .spyOn(teamsService, 'getTeamsList')
         .mockResolvedValueOnce(mockResult);
 
-      expect(await teamsController.getTeamsList(mockRequest)).toEqual({
+      expect(await teamsController.getTeamsList(userId, username)).toEqual({
         teams: mockResult,
         username,
       });
@@ -64,7 +59,7 @@ describe('TeamsController', () => {
         .spyOn(teamsService, 'getTeamsList')
         .mockResolvedValueOnce(mockResult);
 
-      expect(await teamsController.getTeamsList(mockRequest)).toEqual({
+      expect(await teamsController.getTeamsList(userId, username)).toEqual({
         teams: mockResult,
         username,
       });
@@ -75,7 +70,7 @@ describe('TeamsController', () => {
       jest
         .spyOn(teamsService, 'getTeamsList')
         .mockRejectedValueOnce(new Error());
-      await expect(teamsController.getTeamsList(mockRequest)).rejects.toThrow(
+      await expect(teamsController.getTeamsList(userId, username)).rejects.toThrow(
         new HttpException(
           'Failed to get teams',
           HttpStatus.INTERNAL_SERVER_ERROR,
@@ -89,7 +84,7 @@ describe('TeamsController', () => {
     it('should call the resetTeamsList method from teams service', async () => {
       jest.spyOn(teamsService, 'resetTeamsList').mockResolvedValueOnce();
 
-      expect(await teamsController.resetTeamsList(mockRequest)).toBeUndefined();
+      expect(await teamsController.resetTeamsList(userId)).toBeUndefined();
       expect(teamsService.resetTeamsList).toHaveBeenCalledWith(userId);
     });
 
@@ -97,7 +92,7 @@ describe('TeamsController', () => {
       jest
         .spyOn(teamsService, 'resetTeamsList')
         .mockRejectedValueOnce(new Error());
-      await expect(teamsController.resetTeamsList(mockRequest)).rejects.toThrow(
+      await expect(teamsController.resetTeamsList(userId)).rejects.toThrow(
         new HttpException(
           'Failed to reset teams',
           HttpStatus.INTERNAL_SERVER_ERROR,
