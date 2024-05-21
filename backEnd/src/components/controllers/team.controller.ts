bff247c5a6a3647147b196edfac3ba7f4d75fcd9
Gruvix <timmy.burton@hotmail.com>
2024-05-21 15:39:44 -0300

    Improve parameters injection with decorators in controller

--- backEnd/src/components/controllers/team.controller.ts
+++ backEnd/src/components/controllers/team.controller.ts
@@ -2,46 +2,39 @@ import {
   Controller,
   Patch,
   Get,
-  Req,
-  Param,
   Body,
   UseGuards,
   Delete,
   Put,
 } from '@nestjs/common';
-import CustomRequest from '@comp/interfaces/CustomRequest.interface';
-import TeamData from '@comp/interfaces/TeamData.interface';
 import { AuthGuard } from '@comp/guards/auth.guard';
 import { TeamGuard } from '@comp/guards/team.guard';
 import TeamService from '@comp/services/team.service';
 import { UserId } from '@comp/decorators/userId.decorator';
+import { TeamId } from '@comp/decorators/teamId.decorator';
+import TeamData from '@comp/interfaces/TeamData.interface';
 import TeamDTO from '@comp/interfaces/teamDTO.interface';
 
 @UseGuards(AuthGuard, TeamGuard)
 @Controller('user/team/:teamId')
 export default class TeamController {
-  constructor(
-    private readonly teamService: TeamService,
-  ) {}
+  constructor(private readonly teamService: TeamService) {}
 
   @Get()
   async getTeam(
-    @Req() req: CustomRequest,
-    @Param() params: any,
+    @UserId() userId: number,
+    @TeamId() teamId: number,
   ): Promise<TeamDTO> {
-    const { userId } = req.session;
-    const { teamId } = params;
     console.log(`User ${userId} requested team ${teamId}`);
     return await this.teamService.getTeam(teamId);
   }
 
   @Patch()
   async updateTeam(
-    @Param() params: any,
-    @Body() data: TeamData,
     @UserId() userId: number,
+    @TeamId() teamId: number,
+    @Body() data: TeamData,
   ): Promise<void> {
-    const { teamId } = params;
     console.log(`User ${userId} is updating team ${teamId}`);
 
     await this.teamService.updateTeam(teamId, data);
@@ -49,21 +42,18 @@ export default class TeamController {
 
   @Delete()
   async deleteTeam(
-    @Param() params: any,
     @UserId() userId: number,
+    @TeamId() teamId: number,
   ): Promise<void> {
-    const { teamId } = params;
     console.log(`User ${userId} is deleting team ${teamId}`);
     await this.teamService.deleteTeam(teamId);
   }
 
   @Put()
   async resetTeam(
-    @Req() req: CustomRequest,
-    @Param() params: any,
+    @UserId() userId: number,
+    @TeamId() teamId: number,
   ): Promise<void> {
-    const { userId } = req.session;
-    const { teamId } = params;
     console.log(`User ${userId} is resetting team ${teamId}`);
     await this.teamService.resetTeam(teamId);
   }
