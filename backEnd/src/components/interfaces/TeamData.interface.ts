7830c4d9b61f122d5f214ec84695a555b6a53b67
Gruvix <timmy.burton@hotmail.com>
2024-05-21 12:17:04 -0300

    add missing properties to TeamData interface

--- backEnd/src/components/interfaces/TeamData.interface.ts
+++ backEnd/src/components/interfaces/TeamData.interface.ts
@@ -1,4 +1,7 @@
+import Player from "@comp/entities/player.entity";
+
 export default interface TeamData {
+    id: number
     name: string;
     area: string;
     address: string;
@@ -6,5 +9,8 @@ export default interface TeamData {
     website: string;
     email: string;
     venue: string;
+    crestUrl: string;
+    hasCustomCrest: boolean;
+    squad?: Player[];
     readonly defaultTeam: number;
 }
