cd20437843d627bcf9f0906de43005660d064681
Gruvix <timmy.burton@hotmail.com>
2024-05-23 16:36:02 -0300

    update TeamCard class with correct properties
    replace isDefault with hasDefault
    remove lastUpdated

--- frontEnd/src/components/adapters/TeamCard.ts
+++ frontEnd/src/components/adapters/TeamCard.ts
@@ -4,14 +4,12 @@ export default class TeamCard {
   id: number;
   name: string;
   crestUrl: string;
-  isDefault: boolean;
-  lastUpdated: string;
+  hasDefault: boolean;
   [key: string]: number | string | boolean;
   constructor(teamData: any) {
     this.id = teamData.id;
     this.name = teamData.name;
     this.crestUrl = teamData.hasCustomCrest ? `${BASE_API_URL}${teamData.crestUrl}` : teamData.crestUrl;
-    this.isDefault = teamData.hasDefault;
-    this.lastUpdated = teamData.lastUpdated;
+    this.hasDefault = teamData.hasDefault;
   }
 }
