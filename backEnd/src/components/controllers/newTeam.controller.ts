b9c54123b1f52e723981ee08957cb5f99b61101c
Gruvix <timmy.burton@hotmail.com>
2024-05-28 12:37:38 -0300

    Improve new team controller with decorators
    Improve code clarity, maintaining main functionalities as they are, slightly improving overall readability

--- backEnd/src/components/controllers/newTeam.controller.ts
+++ backEnd/src/components/controllers/newTeam.controller.ts
@@ -1,6 +1,5 @@
 import {
   Controller,
-  Req,
   Body,
   UseGuards,
   Post,
@@ -9,37 +8,38 @@ import {
   HttpException,
   Res,
 } from '@nestjs/common';
-import CustomRequest from '@comp/interfaces/CustomRequest.interface';
 import { AuthGuard } from '@comp/guards/auth.guard';
 import TeamService from '@comp/services/team.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import multerOptions from '@comp/storage/multerConfig';
 import { Response } from 'express';
+import { UserId } from '@comp/decorators/userId.decorator';
 
 @UseGuards(AuthGuard)
 @Controller('user/team')
 export default class NewTeamController {
-  constructor(private readonly teamService: TeamService) {}
+  constructor(
+    private readonly teamService: TeamService,
+  ) {}
 
   @Post('add')
   @UseInterceptors(FileInterceptor('image', multerOptions))
   async addTeam(
-    @Req() req: CustomRequest,
+    @UserId() userId: number,
     @Body() body: { teamData: string },
     @UploadedFile() image: Express.Multer.File,
     @Res() res: Response,
   ) {
-    const { username } = req.session;
     const parsedTeamData = JSON.parse(body.teamData);
     console.log(
-      `User ${username} is adding the new team "${parsedTeamData.name}"`,
+      `User ${userId} is adding the new team "${parsedTeamData.name}"`,
     );
     try {
       const newTeamId = await this.teamService.addTeam(
-        username,
+        userId,
         parsedTeamData,
         image.filename,
-      )
+      );
       res.status(201).send(newTeamId.toString());
     } catch (error) {
       if (error instanceof HttpException) {
