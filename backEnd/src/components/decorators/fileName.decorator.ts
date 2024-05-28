a6a4881a27ffac694a1c8a29212a2350342449e0
Gruvix <timmy.burton@hotmail.com>
2024-05-28 12:38:17 -0300

    Create fileName.decorator.ts
    create decorator to inject from request parameters fileName constant

---
+++ backEnd/src/components/decorators/fileName.decorator.ts
@@ -0,0 +1,19 @@
+import {
+    ExecutionContext,
+    HttpException,
+    HttpStatus,
+    createParamDecorator,
+  } from '@nestjs/common';
+  
+  export const FileName = createParamDecorator(
+    (data: unknown, context: ExecutionContext): number => {
+      const request = context.switchToHttp().getRequest();
+      const fileName = request.params.fileName;
+  
+      if (!fileName) {
+        throw new HttpException('Image file name is required', HttpStatus.BAD_REQUEST);
+      }
+      return fileName;
+    },
+  );
+  
