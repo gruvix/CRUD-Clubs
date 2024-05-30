9eb107bbc74fb0ddedad3b76a05369f75a41d88f
Gruvix <timmy.burton@hotmail.com>
2024-05-30 11:02:12 -0300

    Update crestStorage adapter to implement userId
    Implement user id on crest storage operations instead of username, to align with updated folder creation system

--- backEnd/src/components/Adapters/crestStorage.adapter.ts
+++ backEnd/src/components/Adapters/crestStorage.adapter.ts
@@ -1,14 +1,15 @@
-import { deleteFile, readFile } from '../storage/dataStorage';
+import { createFolder, deleteFile, readFile } from '../storage/dataStorage';
 import { getUserRootPath } from '../storage/userPath';
 
 export default class CrestStorageAdapter {
-  async getCrest(username: string, filename: string): Promise<Buffer> {
-    const imgPath = `${getUserRootPath(username)}/${filename}`;
+  async getCrest(userId: number, filename: string): Promise<Buffer> {
+    const imgPath = `${getUserRootPath(userId)}/${filename}`;
     const file = await readFile(imgPath);
     return file;
   }
-  async deleteCrest(username: string, fileName: string) {
-    const imgPath = `${getUserRootPath(username)}/${fileName}`;
+  async deleteCrest(userId: number, fileName: string): Promise<void> {
+    const imgPath = `${getUserRootPath(userId)}/${fileName}`;
+    console.log('trying to delete image on path', imgPath);
     await deleteFile(imgPath);
   }
   async clearCrestFolder(userId: number): Promise<void> {
