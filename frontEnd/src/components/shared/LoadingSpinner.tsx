24554f3d254fca2d919f7c3c66fa8c590eebc2ee
Gruvix <timmy.burton@hotmail.com>
2024-05-30 10:53:59 -0300

    Improve general loading spinner style

--- frontEnd/src/components/shared/LoadingSpinner.tsx
+++ frontEnd/src/components/shared/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function LoadingSpinner({ style }: { style?: React.CSSProperties }) {
   return (
-    <div className="spinner-border text-primary" role="status" style={style}>
+    <div className="spinner-border text-primary spinner-black" role="status" style={style}>
       <span className="visually-hidden">Loading...</span>
     </div>
   );
