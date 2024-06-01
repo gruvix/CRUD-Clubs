95c801d5d7f5b44f0a93af77824f5d8e62121db4
Gruvix <timmy.burton@hotmail.com>
2024-06-01 12:49:43 -0300

    Refactor crest controller, migrate to Nest responses

--- backEnd/src/components/controllers/crest.controller.ts
+++ backEnd/src/components/controllers/crest.controller.ts
@@ -2,22 +2,20 @@ import {
   Controller,
   Get,
   HttpException,
+  HttpStatus,
   Put,
-  Res,
-  UploadedFile,
+  StreamableFile,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from '@comp/guards/auth.guard';
+import { TeamGuard } from '@comp/guards/team.guard';
 import CrestService from '@comp/services/crest.service';
-import { Response } from 'express';
 import multerOptions from '@comp/storage/multerConfig';
 import { UserId } from '@comp/decorators/userId.decorator';
 import { TeamId } from '@comp/decorators/teamId.decorator';
 import { FileName } from '@comp/decorators/fileName.decorator';
-import { Username } from '@comp/decorators/username.decorator';
-import { TeamGuard } from '@comp/guards/team.guard';
 
 @UseGuards(AuthGuard, TeamGuard)
 @Controller('user/customCrest')
@@ -29,11 +27,14 @@ export default class CrestController {
     @UserId() userId: number,
     @TeamId() teamId: number,
     @FileName() fileName: string,
-    @Res() res: Response,
-  ) {
-    console.log(`User ${userId} is requesting crest for team ${teamId}`);
-    const file = await this.crestService.getCrest(userId, fileName);
-    res.send(file);
+  ): Promise<StreamableFile> {
+    try{
+      console.log(`User ${userId} is requesting crest for team ${teamId}`);
+      const fileBuffer = await this.crestService.getCrest(userId, fileName);
+      return new StreamableFile(fileBuffer);
+    } catch (error) {
+      throw new HttpException('Failed to get crest', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Put(':teamId')
@@ -42,8 +43,7 @@ export default class CrestController {
     @UserId() userId: number,
     @TeamId() teamId: number,
     @UploadedFile() image: Express.Multer.File,
-    @Res() res: Response,
-  ) {
+  ): Promise<string> {
     console.log(`User ${userId} is updating crest for team ${teamId}`);
     try {
       const newCrestUrl = await this.crestService.updateCrest(
@@ -51,13 +51,15 @@ export default class CrestController {
         teamId,
         image.filename,
       );
-      res.send(JSON.stringify(newCrestUrl));
+      return JSON.stringify(newCrestUrl);
     } catch (error) {
       if (error instanceof HttpException) {
-        return res.status(error.getStatus()).send(error.message);
+        throw new HttpException(error.message, error.getStatus());
       } else {
-        console.log(error.message);
-        return res.status(500).send(error.message);
+        throw new HttpException(
+          'Failed to update crest',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
       }
     }
   }
