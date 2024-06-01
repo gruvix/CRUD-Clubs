ae54a4d69e5d21eaa5d27df2b9677f8b7d8b7141
Gruvix <timmy.burton@hotmail.com>
2024-06-01 18:40:01 -0300

    Update session storage engine for better performance
    replace session-file-store for express-session-sqlite

--- backEnd/src/components/modules/session.module.ts
+++ backEnd/src/components/modules/session.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
-import { SessionModule as SessionModuleCore} from 'nestjs-session';
+import { SessionModule as SessionModuleCore } from 'nestjs-session';
+import * as sqlite3 from 'sqlite3';
 import * as session from 'express-session';
-import * as FileStore from 'session-file-store';
-import { getSessionsFolderPath } from 'src/components/storage/userPath';
+import sqliteStoreFactory from 'express-session-sqlite';
 
-
+const SqliteStore = sqliteStoreFactory(session);
+const store = new SqliteStore({
+  driver: sqlite3.Database,
+  path: process.env.SESSION_DB_PATH,
+  ttl: 1000 * 60 * 60 * 24,
+});
+console.log(process.env.SESSION_DB_PATH)
 @Module({
   imports: [
     SessionModuleCore.forRoot({
       session: {
-        store: new (FileStore(session))({
-          path: getSessionsFolderPath(),
-          ttl: 60 * 60 * 24,
-          logFn: () => {},
-          reapInterval: 60 * 60,
-        }),
+        store: store,
         secret: process.env.SECRET_KEY,
         resave: false,
         saveUninitialized: true,
