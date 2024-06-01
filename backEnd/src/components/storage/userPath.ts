2b7851b4f28455d9bd5329749be60deed009dd5a
Gruvix <timmy.burton@hotmail.com>
2024-06-01 18:40:14 -0300

    remove deprecated getSessionsFolderPath

--- backEnd/src/components/storage/userPath.ts
+++ backEnd/src/components/storage/userPath.ts
@@ -10,11 +10,6 @@ export function getUserRootPath(userId: string | number) {
   const path = join(projectRoot, "src", "userData", userId.toString());
   return path;
 }
-export function getSessionsFolderPath() {
-  const projectRoot = existsSync(join(__dirname, "..", "sessions")) ? join(__dirname, '..') : process.cwd();
-  const path = join(projectRoot, "src", "sessions");
-  return path;
-}
 export function generateCustomCrestUrl (teamId: number, filename: string) { 
   return `/user/customCrest/${teamId}/${filename}`
 }
