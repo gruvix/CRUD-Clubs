0ff3ea6f6644cf0d99b5104bc1b1dab7c4378f9a
Gruvix <timmy.burton@hotmail.com>
2024-06-07 11:58:27 -0300

    Add unit tests for crest service methods

--- backEnd/src/components/services/__tests__/crest.service.test.ts
+++ backEnd/src/components/services/__tests__/crest.service.test.ts
@@ -1,34 +1,118 @@
 import { Test } from '@nestjs/testing';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import PlayerService from '@comp/services/player.service';
-import UserService from '@comp/services/user.service';
 import TeamService from '@comp/services/team.service';
-import TeamsService from '@comp/services/teams.service';
 import CrestService from '@comp/services/crest.service';
 import { TestSetupModule } from '@comp/testing/testSetup.module';
+import { readFile } from 'fs/promises';
+import CrestStorageService from '@comp/services/crestStorage.service';
+import * as path from '@comp/storage/userPath';
+import Team from '@comp/entities/team.entity';
 
 describe('CrestController', () => {
-  let userService: UserService;
   let teamService: TeamService;
-  let teamsService: TeamsService;
-  let playerService: PlayerService;
   let crestService: CrestService;
   let crestStorageService: CrestStorageService;
   const userId = 1;
   const teamId = 1;
-  const imageFileName = 'image.jpg';
+  const crestFileName = 'image.jpg';
+  const oldCrestFileName = 'iAmOld';
 
   jest.spyOn(console, 'log').mockImplementation(jest.fn());
-  
+
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       imports: [TestSetupModule],
     }).compile();
-    teamsService = module.get<TeamsService>(TeamsService);
-    userService = module.get<UserService>(UserService);
     teamService = module.get<TeamService>(TeamService);
-    playerService = module.get<PlayerService>(PlayerService);
     crestService = module.get<CrestService>(CrestService);
     crestStorageService = module.get<CrestStorageService>(CrestStorageService);
+  });
+
+  describe('getCrest', () => {
+    it('should return a crest', async () => {
+      const fixtureImagePath =
+        './src/components/testing/__fixtures__/image.jpg';
+      const imageFile = await readFile(fixtureImagePath);
+      jest
+        .spyOn(crestStorageService, 'getCrest')
+        .mockResolvedValueOnce(imageFile);
+
+      expect(await crestService.getCrest(userId, crestFileName)).toEqual(
+        imageFile,
+      );
+      expect(crestStorageService.getCrest).toHaveBeenCalledWith(
+        userId,
+        crestFileName,
+      );
+    });
+
+    it('should handle errors', async () => {
+      jest
+        .spyOn(crestStorageService, 'getCrest')
+        .mockRejectedValueOnce(new Error("i'm an error"));
+
+      await expect(
+        crestService.getCrest(userId, crestFileName),
+      ).rejects.toThrow(new Error("i'm an error"));
+    });
+  });
+
+  describe('updateCrest', () => {
+    it('should update the image of the team', async () => {
+      const newCrestUrl = 'i/am/a/url';
+      const relations = [] as string[];
+      const selections = ['crestFileName', 'hasCustomCrest'];
+      jest.spyOn(teamService, 'getTeam').mockResolvedValueOnce({
+        id: teamId,
+        crestFileName: oldCrestFileName,
+        hasCustomCrest: true,
+      } as Team);
+      jest
+        .spyOn(path, 'generateCustomCrestUrl')
+        .mockImplementationOnce(() => newCrestUrl);
+      jest.spyOn(teamService, 'updateTeam').mockResolvedValueOnce(void 0);
+      jest
+        .spyOn(crestStorageService, 'deleteCrest')
+        .mockResolvedValueOnce(void 0);
+
+      expect(
+        await crestService.updateCrest(userId, teamId, crestFileName),
+      ).toEqual(newCrestUrl);
+
+      expect(path.generateCustomCrestUrl).toHaveBeenCalledWith(
+        teamId,
+        crestFileName,
+      );
+      expect(teamService.getTeam).toHaveBeenCalledWith(
+        teamId,
+        relations,
+        selections,
+      );
+      expect(teamService.updateTeam).toHaveBeenCalledWith(teamId, {
+        id: teamId,
+        crestUrl: newCrestUrl,
+        crestFileName: crestFileName,
+        hasCustomCrest: true,
+      } as Team);
+      expect(crestStorageService.deleteCrest).toHaveBeenCalledWith(
+        userId,
+        oldCrestFileName,
+      );
+    });
+
+    it('should delete the new image if there are errors', async () => {
+      jest.spyOn(teamService, 'getTeam').mockImplementationOnce(() => {
+        throw new Error("i'm an error");
+      });
+      jest
+        .spyOn(crestStorageService, 'deleteCrest')
+        .mockResolvedValueOnce(void 0);
+      await expect(
+        crestService.updateCrest(userId, teamId, crestFileName),
+      ).rejects.toThrow("i'm an error");
+      expect(crestStorageService.deleteCrest).toHaveBeenCalledWith(
+        userId,
+        crestFileName,
+      );
+    });
   });
 });
