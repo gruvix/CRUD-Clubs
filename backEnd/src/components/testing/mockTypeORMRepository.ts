ed54d7d01fefad9bc35586ad37fa610acafb58f6
Gruvix <timmy.burton@hotmail.com>
2024-06-12 11:56:23 -0300

    Add createQueryBuilder and related repository mocks

--- backEnd/src/components/testing/mockTypeORMRepository.ts
+++ backEnd/src/components/testing/mockTypeORMRepository.ts
@@ -1,9 +1,19 @@
 const mockRepository = {
   findOneBy: jest.fn(),
+  findOne: jest.fn(),
   manager: {
     transaction: jest.fn(),
     save: jest.fn(),
-  }
+  },
+  createQueryBuilder: jest.fn().mockReturnThis(),
+  insert: jest.fn().mockReturnThis(),
+  into: jest.fn().mockReturnThis(),
+  values: jest.fn().mockReturnThis(),
+  delete: jest.fn().mockReturnThis(),
+  set: jest.fn().mockReturnThis(),
+  update: jest.fn().mockReturnThis(),
+  where: jest.fn().mockReturnThis(),
+  execute: jest.fn().mockReturnThis(),
 };
 
 export default mockRepository;
