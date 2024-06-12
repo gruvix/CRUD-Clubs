d5f9060aeaeeb423736bba3376a08e824a6c5140
Gruvix <timmy.burton@hotmail.com>
2024-06-12 11:58:18 -0300

    Lay groundwork for player service unit testing

---
+++ backEnd/src/components/services/__tests__/player.service.test.ts
@@ -0,0 +1,38 @@
+import { Test } from '@nestjs/testing';
+import { TestSetupModule } from '@comp/testing/testSetup.module';
+import MockTestUtils from '@comp/testing/MockTestUtils';
+import PathTestUtils from '@comp/testing/PathTestUtils';
+import mockRepository from '@comp/testing/mockTypeORMRepository';
+import * as TypeORM from '@nestjs/typeorm';
+import PlayerService from '../player.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+describe('PlayerService', () => {
+  let playerService: PlayerService;
+  const mocks = new MockTestUtils();
+  const mockPaths = new PathTestUtils();
+
+  const mockGetRepositoryToken = jest
+    .fn()
+    .mockReturnValue(Symbol('MockedRepositoryToken'));
+
+  jest
+    .spyOn(TypeORM, 'getRepositoryToken')
+    .mockImplementation(mockGetRepositoryToken);
+  jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [TestSetupModule],
+      providers: [
+        {
+          provide: Symbol('MockedRepositoryToken'),
+          useValue: mockRepository,
+        },
+      ],
+    }).compile();
+
+    playerService = module.get<PlayerService>(PlayerService);
+  });
+
+});
