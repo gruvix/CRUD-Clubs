d5f4849c57ac71eed927c9ea7bb381707d064357
Gruvix <timmy.burton@hotmail.com>
2024-06-13 12:35:15 -0300

    Lay groundwork for team service unit testing

---
+++ backEnd/src/components/services/__tests__/team.service.test.ts
@@ -0,0 +1,42 @@
+import { Test } from '@nestjs/testing';
+import { TestSetupModule } from '@comp/testing/testSetup.module';
+import MockTestUtils from '@comp/testing/MockTestUtils';
+import PathTestUtils from '@comp/testing/PathTestUtils';
+import mockRepository from '@comp/testing/mockTypeORMRepository';
+import * as TypeORM from '@nestjs/typeorm';
+import PlayerService from '../player.service';
+import CrestStorageService from '../crestStorage.service';
+
+describe('TeamService', () => {
+  let playerService: PlayerService;
+  let crestStorageService: CrestStorageService;
+
+  const mocks = new MockTestUtils();
+  const mockPaths = new PathTestUtils();
+
+  const mockGetRepositoryToken = jest
+    .fn()
+    .mockReturnValue(Symbol('MockedRepositoryToken'));
+
+  jest
+    .spyOn(TypeORM, 'getRepositoryToken')
+    .mockImplementation(mockGetRepositoryToken);
+  jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [TestSetupModule],
+      providers: [
+        {
+          provide: Symbol('MockedRepositoryToken'),
+          useValue: mockRepository,
+        },
+      ],
+    }).compile();
+
+    crestStorageService = module.get<CrestStorageService>(CrestStorageService);
+    playerService = module.get<PlayerService>(PlayerService);
+  });
+
+ 
+});
