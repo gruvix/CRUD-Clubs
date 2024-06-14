55aaec628583bd1b203f9d7e9c2e26a3489e8a8a
Gruvix <timmy.burton@hotmail.com>
2024-06-14 14:53:35 -0300

    Add unit tests for copyTeamsToUser in teams service

--- backEnd/src/components/services/__tests__/teams.service.test.ts
+++ backEnd/src/components/services/__tests__/teams.service.test.ts
@@ -11,6 +11,7 @@ import TeamService from '../team.service';
 import CrestStorageService from '../crestStorage.service';
 import TeamShortDTO from '@comp/interfaces/TeamShortDTO.interface';
 import TeamShort from '@comp/models/TeamShort';
+import Team from '@comp/entities/team.entity';
 
 describe('TeamService', () => {
   let teamsService: TeamsService;
@@ -49,17 +50,20 @@ describe('TeamService', () => {
   describe('getTeamsList', () => {
     it('Should return list of teams', async () => {
       const teamsAmount = 5;
-      let databaseTeamShorts = mocks.generateTeamArray<TeamShort>(teamsAmount, 'TeamShort');
+      let databaseTeamShorts = mocks.generateTeamArray<TeamShort>(
+        teamsAmount,
+        'TeamShort',
+      );
       let expectedTeamDTOs = databaseTeamShorts.map((team) => {
         return mocks.transformTeamShortToDTO(team);
       });
 
-      jest.spyOn(mockRepository, 'getMany').mockResolvedValue(databaseTeamShorts);
+      jest
+        .spyOn(mockRepository, 'getMany')
+        .mockResolvedValue(databaseTeamShorts);
 
-      const teamsList = await teamsService.getTeamsList(mocks.userId)
-      expect(teamsList).toEqual(
-        expectedTeamDTOs,
-      );
+      const teamsList = await teamsService.getTeamsList(mocks.userId);
+      expect(teamsList).toEqual(expectedTeamDTOs);
       expect(teamsList.length).toBe(teamsAmount);
       expect(mockRepository.where).toHaveBeenCalledWith('team.user = :userId', {
         userId: mocks.userId,
@@ -70,9 +74,7 @@ describe('TeamService', () => {
     it('Should return an empty list of teams', async () => {
       jest.spyOn(mockRepository, 'getMany').mockResolvedValue([]);
       const teamsList = await teamsService.getTeamsList(mocks.userId);
-      expect(teamsList).toEqual(
-        expect.arrayContaining<TeamShortDTO>([]),
-      );
+      expect(teamsList).toEqual(expect.arrayContaining<TeamShortDTO>([]));
       expect(teamsList.length).toBe(0);
     });
 
@@ -89,6 +91,42 @@ describe('TeamService', () => {
       await expect(teamsService.getTeamsList(mocks.userId)).rejects.toThrow(
         new Error('Something went wrong'),
       );
-    })
+    });
+  });
+
+  describe('copyTeamsToUser', () => {
+    const user = mocks.userEntity();
+    it('Should store the teams in the user object', async () => {
+      const amount = 5;
+      const teams = mocks.generateTeamArray<Team>(amount, 'Team', false);
+      const squad = mocks.squadGenerator(mocks.teamId, amount);
+      jest.spyOn(teamService, 'getTeam').mockResolvedValue({ squad } as Team);
+      jest
+        .spyOn(playerService, 'copyPlayersToTeam')
+        .mockImplementationOnce((team, squad) => (team.squad = squad));
+
+      expect(await teamsService.copyTeamsToUser(user, teams)).toBeUndefined();
+      expect(user.teams.length).toBe(amount);
+      expect(user.teams[0].squad).toEqual(squad);
+    });
+
+    it('Should store an empty array of teams on the user object', async () => {
+      const teams = [];
+
+      expect(await teamsService.copyTeamsToUser(user, teams)).toBeUndefined();
+      expect(user.teams.length).toBe(0);
+    });
+
+    it('Should handle errors', async () => {
+      const teams = mocks.generateTeamArray<Team>(1, 'Team', false);
+
+      jest
+        .spyOn(teamService, 'getTeam')
+        .mockRejectedValueOnce(new Error('Something went wrong'));
+
+      expect(teamsService.copyTeamsToUser(user, teams)).rejects.toThrow(
+        new Error('Something went wrong'),
+      );
+    });
   });
 });
