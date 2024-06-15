b9396fe1fcf7ea4900e9ed2f8f3ce4a54e87d6f6
Gruvix <timmy.burton@hotmail.com>
2024-06-15 13:20:31 -0300

    Add entity generators for readJSONFile mocks

--- backEnd/src/components/testing/MockEntities.ts
+++ backEnd/src/components/testing/MockEntities.ts
@@ -1,3 +1,4 @@
+import DefaultTeam from '@comp/entities/defaultTeam.entity';
 import Player from '@comp/entities/player.entity';
 import Team from '@comp/entities/team.entity';
 import User from '@comp/entities/user.entity';
@@ -65,7 +66,7 @@ export default class MockEntities {
       }),
     );
   }
-  
+
   user(userId: number = this.userId): User {
     return JSON.parse(
       JSON.stringify({
@@ -130,5 +131,55 @@ export default class MockEntities {
       defaultTeam: isDefault ? teamId : undefined,
       hasCustomCrest: !isDefault,
     });
+  }
+  teamsJSON(teamsAmount: number = 10): { [key: number]: Object } {
+    const teams: { [key: number]: Object } = {};
+    for (let i = 0; i < teamsAmount; i++) {
+      // Generate random ID (replace with your preferred ID generation logic)
+      const id = Math.floor(Math.random() * 1000) + 1;
+      teams[id] = { id };
+    }
+    return teams;
+  }
+
+  teamJSON(teamId: number): TeamJSON {
+    return JSON.parse(
+      JSON.stringify({
+        id: teamId,
+        name: 'testName',
+        area: { id: 0, name: 'testArea' },
+        address: 'testAdress',
+        phone: 'testPhone',
+        website: 'testWebsite',
+        email: 'testEmail',
+        venue: 'testVenue',
+        crestUrl: 'test.url',
+        founded: 2000,
+        clubColors: 'test / Colors',
+        lastUpdated: new Date(),
+        squad: [],
+      } as TeamJSON),
+    );
+  }
+
+  squadJSONGenerator(playersAmount: number): PlayerJSON[] {
+    const squad: PlayerJSON[] = [];
+    for (let i = 0; i < playersAmount; i++) {
+      squad.push({
+        id: i + 1,
+        name: 'They call me test',
+        position: i ? null : 'somewhere', //the first player is the coach, no position
+        role: 'something',
+        nationality: 'someplace',
+      } as PlayerJSON);
+    }
+    return squad;
+  }
+
+  defaultTeam(team: Team): DefaultTeam {
+    return {
+      id: team.id,
+      teams: [team],
+    };
   }
 }
