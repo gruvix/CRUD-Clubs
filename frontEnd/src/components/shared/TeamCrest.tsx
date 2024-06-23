78b22030d8b78ba9aaf7dceb0c859ab0076ac157
Gruvix <timmy.burton@hotmail.com>
2024-06-23 15:52:39 -0300

    Add spinn to crest on click

--- frontEnd/src/components/shared/TeamCrest.tsx
+++ frontEnd/src/components/shared/TeamCrest.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from "react";
 interface TeamCrestProps {
   teamCrest: string;
   className: string;
 }
 
 export default function TeamCrest({ teamCrest, className }: TeamCrestProps) {
+  const [spin, setSpin] = React.useState<boolean>(false);
   return (
-    <img src={teamCrest} className={className} alt="team crest" id="team-crest" />
+    <img
+      src={teamCrest}
+      className={className + ` transition-transform duration-300 ease-in-out rotate-[${spin ? 360 : 0}deg]`}
+      alt="team crest"
+      id="team-crest"
+      onClick={() => setSpin((previousState) => !previousState)}
+    />
   );
 }
