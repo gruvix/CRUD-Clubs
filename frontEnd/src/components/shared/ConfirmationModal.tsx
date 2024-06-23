f4845ecc980e01828629b1c9bf3308099ec8b960
Gruvix <timmy.burton@hotmail.com>
2024-06-23 13:14:07 -0300

    Fix modal not cancelling when hidden not by button
    When modal gets hidden it will call cancel callback even if the cancel button wasn't clicked, that means when ESCAPE is pressed or other part of the page gets clicked
    this fixes modal getting hidden and cancel not happening, leaving confirmation preparations ready

--- frontEnd/src/components/shared/ConfirmationModal.tsx
+++ frontEnd/src/components/shared/ConfirmationModal.tsx
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-export default function ConfirmationModal({ callback = () => {}, confirmationText, cancelCallback = () => {} }: { callback?: () => void, confirmationText: string, cancelCallback?: Function }) {
+export default function ConfirmationModal({
+  callback = () => {},
+  confirmationText,
+  cancelCallback = () => {},
+}: {
+  callback?: () => void;
+  confirmationText: string;
+  cancelCallback?: Function;
+}) {
+  useEffect(() => {
+    const myModal = document.getElementById("confirmationModal");
+
+    const handleHiddenEvent = () => {
+      cancelCallback();
+    };
+    myModal?.addEventListener("hidden.bs.modal", handleHiddenEvent);
+
+    return () => {
+      myModal?.removeEventListener("hidden.bs.modal", handleHiddenEvent);
+    };
+  }, [cancelCallback]);
   return (
-    <div className="modal fade" id="confirmationModal" tabIndex={-1} aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div
+      className="modal fade"
+      id="confirmationModal"
+      tabIndex={-1}
+      aria-labelledby="confirmationModalLabel"
+      aria-hidden="true"
+    >
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h1 className="modal-title fs-5" id="confirmationModalLabel">Are you sure?</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+            <h1 className="modal-title fs-5" id="confirmationModalLabel">
+              Are you sure?
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            />
           </div>
           <div className="modal-body">
             <span id="modal-confirmation-text">{confirmationText}</span>
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-outline-success" id="confirmation-modal-button" data-bs-dismiss="modal" onClick={() => callback()}>Confirm</button>
-            <button type="button" className="btn btn-outline-dark" id="cancel-modal-button" data-bs-dismiss="modal" onClick={() => cancelCallback()}>Cancel</button>
+            <button
+              type="button"
+              className="btn btn-outline-success"
+              id="confirmation-modal-button"
+              data-bs-dismiss="modal"
+              onClick={() => callback()}
+            >
+              Confirm
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-dark"
+              id="cancel-modal-button"
+              data-bs-dismiss="modal"
+              onClick={() => cancelCallback()}
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
